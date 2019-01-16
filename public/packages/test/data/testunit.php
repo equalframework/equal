8886f14b61817dfed8564c7f262bc332dc12ed66
cedricfrancoys <cedricfrancoys@gmail.com>
2019-01-16 17:13:49 +0100

    updated to json output and added auth tests

--- public/packages/test/data/testunit.php
+++ public/packages/test/data/testunit.php
@@ -5,28 +5,28 @@
 *    Copyright (C) Cedric Francoys, 2015, Yegen
 *    Some Rights Reserved, GNU GPL 3 license <http://www.gnu.org/licenses/>
 */
-use config\QNLib as QNLib;
 use qinoa\orm\ObjectManager;
 
 
 use core\User;
 use core\Group;
 
-$params = eQual::announce([
+list($params, $providers) = eQual::announce([
     'description'   => 'Initialise database for core package',
     'params'        => [],
-    'constants'     => ['DB_HOST', 'DB_PORT', 'DB_NAME', 'DB_USER', 'DB_PASSWORD', 'DB_DBMS']
+    'constants'     => ['DB_HOST', 'DB_PORT', 'DB_NAME', 'DB_USER', 'DB_PASSWORD', 'DB_DBMS'],
+    'providers'     => ['context']
 ]);
 
 
-$tests = array(
-            //0xxx : calls related to QNLib methods
+$tests = [
+            //0xxx : calls related to QN methods
             '0101' => array(
                             'description'       => "Get auth provider",
                             'return'            => array('boolean'),
                             'expected'          => true,
                             'test'              => function (){
-                                                        list($params, $providers) = QNLib::announce([
+                                                        list($params, $providers) = eQual::announce([
                                                             'providers' => ['qinoa\auth\AuthenticationManager']
                                                         ]);
                                                         $auth = $providers['qinoa\auth\AuthenticationManager'];
@@ -39,7 +39,7 @@ $tests = array(
                             'return'            => array('boolean'),
                             'expected'          => true,
                             'test'              => function (){
-                                                        list($params, $providers) = QNLib::announce([
+                                                        list($params, $providers) = eQual::announce([
                                                             'providers' => ['@@testAuth' => 'qinoa\auth\AuthenticationManager']
                                                         ]);
                                                         $auth = $providers['@@testAuth'];
@@ -241,78 +241,96 @@ $tests = array(
             
             //24xx : calls related to the remove method            
             '2401' => array(
-                            'description'       => "Create a user (no validation)",
+                            'description'       => "Remove a user (no validation)",
                             'return'            => array('integer', 'array'),
                             'expected'          => array(&$GLOBALS['dummy_user_id']), 
                             'test'              => function () {
                                                         global $dummy_user_id;
                                                         $om = &ObjectManager::getInstance();
-                                                        return $om->remove('core\User', $dummy_user_id);
+                                                        return $om->remove('core\User', $dummy_user_id, true);
                                                     },
                             ),
                             
             //25xx : calls related to the search method
             // @signature : public function search($object_class, $domain=NULL, $order='id', $sort='asc', $start='0', $limit='0', $lang=DEFAULT_LANG) {
             // @return : mixed (integer or array)
-            '2500' => array(
-                            'description'       => "Search an object with valid clause 'ilike'",
+            '2501' => array(
+                            'description'       =>  "Search an object with valid clause 'ilike'",
                             'return'            => array('integer', 'array'),
                             'expected'          => array('1'),
                             'test'              => function (){
                                                         $om = &ObjectManager::getInstance();
                                                         return $om->search('core\Group', array(array(array('name', 'ilike', '%Default%'))));
-                                                    },
+                                                    }
                             ),
-            '2501' => array(
+            '2502' => array(
                             'description'       => "Search an object with invalid clause 'ilike' (non-existing field)",
                             'return'            => array('integer', 'array'),
                             'expected'          => QN_ERROR_INVALID_PARAM,
                             'test'              => function (){
                                                         $om = &ObjectManager::getInstance();
                                                         return $om->search('core\Group', array(array(array('badname', 'ilike', '%Default%'))));
-                                                    },
+                                                    }
                             ),                            
-/*                            
-            '2310' => array(
-                            'description'        => "Trying to search for some object : clause 'contains' on one2many field",
-                            'expected'    => true,
-                            'test'                => function (){
-                                                        $values = search('knine\Article', array(array('attributes_ids', 'contains', array(1, 2))));
-                                                        return $values;
+            '2510' => array(
+                            'description'       => "Search for some object : clause 'contains' on one2many field",
+                            'return'            => array('boolean'),
+                            'expected'          => true,
+                            'test'              => function (){
+// todo                                                        
+                                                        return true;
                                                     },
                             ),
-            '2320' => array(
-                            'description'        => "Trying to search for some object : clause 'contains' on one2many field (using a foreign key different from 'id')",
-                            'expected'    => true,
-                            'test'                => function (){
-                                                        $values = search('knine\Article', array(array('attributes_types', 'contains', array('author', 'editor'))));
-                                                        return $values;
-                                                    },
+            '2520' => array(
+                            'description'       =>  "Search for some object : clause 'contains' on one2many field (using a foreign key different from 'id')",
+                            'return'            => array('boolean'),                            
+                            'expected'          =>  true,
+                            'test'              =>  function (){
+// todo
+                                                        return true;
+                                                    }
                             ),
-            '2330' => array(
-                            'description'        => "Trying to search for some object : clause 'contains' on many2one field",
-                            'expected'    => true,
-                            'test'                => function (){
-                                                        $values = search('knine\Article', array(array('parent_id', '=', '1')));
-                                                        return $values;
-                                                    },
+            '2530' => array(
+                            'description'       =>  "Search for some object : clause 'contains' on many2one field",
+                            'return'            => array('boolean'),                            
+                            'expected'          =>  true,
+                            'test'              =>  function (){
+// todo
+                                                        return true;
+                                                    }
                             ),
-            '2340' => array(
-                            'description'        => "Trying to search for some object : clause contain on many2many field",
-                            'expected'    => true,
-                            'test'                => function (){
-                                                        $values = search('knine\Article', array(array('labels_ids', 'contains', array(1, 2, 3))));
-                                                        return $values;
-                                                }    ,
+                            
+            '2540' => array(
+                            'description'       =>  "Search for some object : clause contain on many2many field",
+                            'return'            => array('integer', 'array'),
+                            'expected'          => array(1 => ['login' => 'root@example.com'], 2 => ['login' => 'cedricfrancoys@gmail.com']),
+                            
+                            'test'              =>  function () use($providers) {
+                                                        try {
+                                                            $providers['context']->container->get('auth')->authenticate('cedricfrancoys@gmail.com', '02e5408967241673cd03126fe55dcd1a');
+                                                            $providers['context']->container->get('access')->grant(QN_R_READ);
+                                                            
+                                                            $values = User::search(array(array('groups_ids', 'contains', array(1, 2, 3))))
+                                                                  ->read(['login'])
+                                                                  ->get();
+                                                        }
+                                                        catch(Exception $e) {
+                                                            // possible raised Exception codes : QN_ERROR_NOT_ALLOWED
+                                                            $values = $e->getCode();
+                                                            
+                                                        }
+                                                        return $values;                                                        
+                                                    }
                             ),
-*/
+
             // 3xxx methods : related to Collections calls
-            '3000' => array(
-                            'description'       => "Search a user object using Collection (result as map)",
+            '3101' => array(
+                            'description'       => "Search for an existing user object using Collection (result as map)",
                             'return'            => array('integer', 'array'),
                             'expected'          => array('2' => ['login' => 'cedricfrancoys@gmail.com']),
                             'test'              => function () {
                                                         try {
+                                                            
                                                             $values = User::search(['login', 'like', 'cedricfrancoys@gmail.com'])
                                                                       ->read(['login'])
                                                                       ->get();
@@ -324,13 +342,13 @@ $tests = array(
                                                         return $values;
                                                 }
                             ),
-            '3001' => array(
-                            'description'       => "Search a user object using Collection (result as array)",
+            '3102' => array(
+                            'description'       => "Search for an existing user object using Collection (result as array)",
                             'return'            => array('integer', 'array'),
                             'expected'          => array(['login' => 'cedricfrancoys@gmail.com']),
                             'test'              => function () {
                                                         try {
-                                                            $values = User::search(['login', 'like', 'cedricfrancoys@gmail.com'])
+                                                            $values = User::search(['login', '=', 'cedricfrancoys@gmail.com'])
                                                                       ->read(['login'])
                                                                       ->get(true);
                                                         }
@@ -340,9 +358,42 @@ $tests = array(
                                                         }
                                                         return $values;
                                                 }
-                            ),                            
+                            ),
 
-);
+            '3103' => array(
+                            'description'       =>  "Search for a new user object using Collection (result as array)",
+                            'return'            =>  array('integer', 'array'),
+                            'expected'          =>  array(['login' => 'test@equal.run']),
+                            'precondition'      =>  function() use($providers) {
+                                                        try {
+                                                            $providers['context']->container->get('access')->grant(QN_R_CREATE|QN_R_DELETE);
+                                                            $values = User::create(['login' => 'test@equal.run', 'password' => md5('test'), 'firstname', 'lastname'])->ids();
+                                                        }
+                                                        catch(\Exception $e) {
+                                                            // possible raised Exception codes : QN_ERROR_NOT_ALLOWED
+                                                            $values = $e->getCode();
+                                                        }
+                                                        return $values;
+                                                    },
+                            'rollback'          =>  function() {
+                                                        User::search(['login', '=', 'test@equal.run'])->delete(true);
+                                                    },
+                            'test'              =>  function () {
+                                                        try {
+                                                            $values = User::search(['login', '=', 'test@equal.run'])
+                                                                      ->read(['login'])
+                                                                      ->get(true);
+                                                        }
+                                                        catch(\Exception $e) {
+                                                            // possible raised Exception codes : QN_ERROR_NOT_ALLOWED
+                                                            $values = $e->getCode();
+                                                        }
+                                                        return $values;
+                                                    }
+                            ),
+
+                            
+];
 
 function array_equals($array1, $array2) {
     $res = true;
@@ -366,56 +417,65 @@ function array_equals($array1, $array2) {
     return $res;
 }
 
-echo "<pre>";
-
+$results = [];
 $fails = false;
 
 foreach($tests as $id => $test) {
-    echo "{ $id } :  {$test['description']} - ";
+    
+    if(isset($test['precondition']) && is_callable($test['precondition'])) {
+        $precondition = $test['precondition']();
+    }
+    
     $result = $test['test']();
-
+    $status = 'ok';
+    
     if(in_array(gettype($result), $test['return'])) {
         
         if(isset($test['expected'])) {
             if(gettype($result) == gettype($test['expected'])) {
                 if(gettype($result) == "array") {
-                    if(array_equals($result, $test['expected'])) {
-                        echo '<b style="color: green;">OK</b>';
-                    }
-                    else {
-                        echo '<b style="color: red;">KO</b>';
-                        $fails = true;
+                    if(!array_equals($result, $test['expected'])) {
+                        $status= 'ko';
                     }
                 }
                 else {
-                    if($result == $test['expected']) {
-                        echo '<b style="color: green;">OK</b>';
-                    }
-                    else {
-                        echo '<b style="color: red;">KO</b>';
-                        $fails = true;                        
+                    if($result != $test['expected']) {
+                        $status = 'ko';
                     }
                 }
             }
             else {
-                echo '<b style="color: red;">KO</b>';
-                $fails = true;
+                $status = 'ko';
             }
-        }
-        else {
-            echo '<b style="color: green;">OK</b>';
         }
     }
     else {
-        echo '<b style="color: red;">KO</b>';
-        $fails = true;
+        $status = 'ko';
     }
-    echo "  (result=".json_encode($result)."), (expected=".json_encode($test['expected']).")";
+//         throw new Exception("invalid test structure", QN_ERROR_INVALID_PARAM);
+    if($status == 'ko') $fails = true;
+    else {
+        if(isset($test['rollback']) && is_callable($test['rollback'])) {
+            $test['rollback']();
+        }        
+    }
+    
+    $results[$id] = [
+        'description'   => $test['description'],
+        'status'        => $status,        
+        'result'        => $result
+    ];
 
-    echo "\n";
+    if(isset($test['expected'])) {
+        $results[$id]['expected'] = $test['expected'];
+    }
+    
+    if(isset($test['precondition'])) {
+        $results[$id]['precondition'] = $precondition;
+    }
 }
 
-echo "</pre>";
+$providers['context']->httpResponse()->body($results)->send();
 
 if($fails) {
     exit(1);
