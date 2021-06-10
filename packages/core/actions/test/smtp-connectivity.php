4bf39de978cd800e3e06e9d86c34aa634dc6fd52
unknown <cedricfrancoys@gmail.com>
2021-06-10 15:56:26 +0200

    added DNS tests

--- packages/core/actions/test/smtp-connectivity.php
+++ packages/core/actions/test/smtp-connectivity.php
@@ -13,7 +13,8 @@ list($params, $providers) = announce([
 global $result;
 $result = '';
 
-// define 
+// SMTP communication utilities
+
 function send_line($sock, $msg, $append=true) {
     global $result;
     $msg = sprintf("%s\r\n", $msg);
@@ -39,27 +40,55 @@ list($host, $port, $username, $password) = [
 ];
 list($account, $domain) = explode('@', EMAIL_SMTP_ACCOUNT_EMAIL);
 
+echo $host, $port, $username, $password;
+
 $attributes = [];
 
-$sock = fsockopen($host, $port, $errno, $errstr, 2);
-stream_set_timeout($sock, 1);
-
-
 try {
+    // check email syntax validity
+    if(filter_var(EMAIL_SMTP_ACCOUNT_EMAIL, FILTER_VALIDATE_EMAIL) === false) {
+        throw new Exception('invalid_smtp_account_email', QN_ERROR_INVALID_PARAM);
+    }
+    
+    // check email domain validity
+    if(filter_var($domain, FILTER_VALIDATE_DOMAIN) === false) {
+        throw new Exception('invalid_smtp_account_domain', QN_ERROR_INVALID_PARAM);
+    }
+
+    // make sure that an MX record is set for domain to which relates the given email account
+    if( checkdnsrr($domain, "MX") === false) {
+        throw new Exception('no_mx_record_for_smtp_account_domain', QN_ERROR_INVALID_PARAM);
+    }
+
+    // make sure the smtp hostname can be resolved to an IP address
+    if( ip2long(EMAIL_SMTP_HOST) === false) {
+        $ipv4 = gethostbyname(EMAIL_SMTP_HOST);
+        if($ipv4 == EMAIL_SMTP_HOST) {
+            throw new Exception('invalid_smtp_host', QN_ERROR_INVALID_PARAM);
+        }    
+    }
+
+    // try to establish a connexion with the SMTP host
+    $sock = fsockopen($host, $port, $errno, $errstr, 2);
+    if (!$sock) {
+        throw new Exception('unable_to_conect', QN_ERROR_UNKNOWN);
+    } 
+    // do not wait for respponses more than 1 sec
+    stream_set_timeout($sock, 1);
+    
     // most MTAs expect a domain/host name, and the picky ones want the hostname specified here
     // to match the reverse lookup of the IP address.
     send_line($sock, sprintf("EHLO %s", $domain));
 
-
     if(!($line = read_line($sock))) {
-        throw new Exception('No response received', QN_ERROR_UNKNOWN);        
+        throw new Exception('no_response_received', QN_ERROR_UNKNOWN);
     }
 
     $matches = [];
     preg_match('/^([0-9]{3})([ -])(.*)$/', $line, $matches);
 
     if( intval($matches[1]) != 220) {
-        throw new Exception('Abnormal EHLO response received: ' . $line, QN_ERROR_UNKNOWN);
+        throw new Exception('invalid_EHLO_response', QN_ERROR_UNKNOWN);
     }
 
     // SMTP responses can span multiple lines, they will look like
@@ -74,7 +103,7 @@ try {
         preg_match('/^([0-9]{3})([ -])(.*)$/', $line, $matches);
 
         if( intval($matches[1]) != 250) {
-            throw new Exception('Abnormal data received: ' . $line, QN_ERROR_UNKNOWN);
+            throw new Exception('invalid_data_received', QN_ERROR_UNKNOWN);
         }
         $parts = explode(' ', $matches[3], 2);
         $attributes[$parts[0]] = (isset($parts[1]))?$parts[1]:'';
@@ -84,7 +113,7 @@ try {
     // The advertised capabilities of the server in the EHLO response will include 
     // the types of AUTH mechanisms that are supported
     if(!isset($attributes['AUTH']) || !in_array($attributes['AUTH'], ['LOGIN PLAIN', 'PLAIN LOGIN']) ) {
-        throw new Exception('Plain login authentication not supported', QN_ERROR_UNKNOWN);
+        throw new Exception('plain_login_auth_not_available', QN_ERROR_UNKNOWN);
     }
 
     // request authentication
@@ -92,7 +121,7 @@ try {
     $line = read_line($sock);
 
     if(!$line || $line != "334 VXNlcm5hbWU6") {
-        throw new Exception('No prompt for username received', QN_ERROR_UNKNOWN);    
+        throw new Exception('no_username_prompt_received', QN_ERROR_UNKNOWN);    
     }
 
     // send username
@@ -101,9 +130,9 @@ try {
 
     if(!$line || $line != "334 UGFzc3dvcmQ6") {
         if(intval(explode(' ', $line)[0]) == 535) {
-            throw new Exception('Authentication failed', QN_ERROR_UNKNOWN);
+            throw new Exception('failed_auth_at_user', QN_ERROR_UNKNOWN);
         }        
-        throw new Exception('No prompt for password received', QN_ERROR_UNKNOWN);
+        throw new Exception('no_password_prompt_received', QN_ERROR_UNKNOWN);
     }
 
 
@@ -112,7 +141,7 @@ try {
     $line = read_line($sock);
 
     if(!$line || intval(explode(' ', $line)[0]) != 235) {
-        throw new Exception("Unable to authenticate with provided credentials: $line", QN_ERROR_UNKNOWN);
+        throw new Exception("failed_auth_at_pass", QN_ERROR_UNKNOWN);
     }
 }
 catch(Exception $e) {
