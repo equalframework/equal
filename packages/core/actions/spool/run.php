42c526c76fed7af8a028c492199c0da8fc1065f2
unknown <cedricfrancoys@gmail.com>
2021-06-30 16:46:48 +0200

    added new controller for sending queued emails (spool)

---
+++ packages/core/actions/spool/run.php
@@ -0,0 +1,31 @@
+<?php
+use core\User;
+
+// announce script and fetch parameters values
+list($params, $providers) = announce([	
+    'description'	=>	"Send emails that are currently in spool queue.",
+    'params' 		=>	[
+    ],
+    'response'      => [
+        'content-type'      => 'application/json',
+        'charset'           => 'utf-8',
+        'accept-origin'     => '*'
+    ],        
+    'providers'     => ['context', 'orm', 'spool', 'auth']
+]);
+
+
+// initalise local vars with inputs
+list($om, $context, $spool, $auth) = [ $providers['orm'], $providers['context'], $providers['spool'], $providers['auth'] ];
+
+// todo : improve permissions
+
+if( $auth->userId() != ROOT_USER_ID) {
+    throw new \Exception("spool_access_restricted", QN_ERROR_NOT_ALLOWED);
+}
+
+$spool->run();
+
+$context->httpResponse()
+        ->body('')
+        ->send();
