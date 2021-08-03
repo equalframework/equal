ae62864009275e95107a29d57395e49422c33b26
Cédric Françoys <cedricfrancoys@gmail.com>
2021-08-03 14:56:17 +0200

    updated license notice

--- lib/equal/db/DBConnection.class.php
+++ lib/equal/db/DBConnection.class.php
@@ -1,9 +1,8 @@
 <?php
 /*
-* KNINE php library
-*
-* DBConnection class
-*
+    This file is part of the eQual framework <http://www.github.com/cedricfrancoys/equal>
+    Some Rights Reserved, Cedric Francoys, 2010-2021
+    Licensed under GNU LGPL 3 license <http://www.gnu.org/licenses/>
 */
 namespace equal\db;
 
