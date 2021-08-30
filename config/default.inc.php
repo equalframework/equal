85145f43d8c331ca9ff0ffd1987292629e1cf5d5
Cédric Françoys <cedricfrancoys@gmail.com>
2021-08-30 11:15:14 +0200

    added some comments about php.ini values

--- config/default.inc.php
+++ config/default.inc.php
@@ -161,8 +161,8 @@ namespace config {
     * File transfer parameters
     */
     // maximum authorized size for file upload (in octet)
-    // keep in mind that this parameter does not override the PHP 'upload_max_filesize' directive
-    // so it can be more restrictive but will not be effective if set higher
+    // keep in mind that this parameter does not override the PHP 'upload_max_filesize' and 'post_max_size' directives.
+    // So it will not be effective if set higher than those.
     // ('upload_max_filesize' and 'post_max_size' are PHP_INI_PERDIR directives and must be defined in php.ini)
 
     define('UPLOAD_MAX_FILE_SIZE', 64*1024*1024);		// set upload limit to 64Mo
