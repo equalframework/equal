1e60d8302614592bf001591b5789a92783475754
Cédric Françoys <cedricfrancoys@gmail.com>
2021-09-29 16:29:45 +0200

    extended support for lang validation

--- lib/equal/data/DataValidator.class.php
+++ lib/equal/data/DataValidator.class.php
@@ -4,27 +4,41 @@ namespace equal\data;
 use equal\organic\Service;
 
 class DataValidator extends Service {
-    
+
     /**
      * Provide a structure corresponding to the given usage.
      * In case no constraint is associated to the usage, an always-valid constraint is returned.
-     * 
+     *
+     * getConstraintFromUsage('language/iso-639')['rule']('fr');        // true
+     * getConstraintFromUsage('language/iso-639')['rule']('fr-CA');     // true
+     * getConstraintFromUsage('language/iso-639')['rule']('en-GB');     // true
+     * getConstraintFromUsage('language/iso-639')['rule']('en-US');     // true
+     * getConstraintFromUsage('language/iso-639')['rule']('fr-');       // false
+     * getConstraintFromUsage('language/iso-639')['rule']('en-en');     // false
+     * getConstraintFromUsage('language/iso-639')['rule']('en-EN');     // false
+     *
      * @param   $usage  string  Identifier of the usage for which the constraint is requested.
      * @return  array
      */
     public static function getConstraintFromUsage($usage) {
         switch($usage) {
             case 'language/iso-639':
-            case 'language/iso-639:2': 
+            case 'language/iso-639:2':
                 return [
-                    'kind'  => 'function', 
+                    'kind'  => 'function',
                     'rule'  => function($a, $o) {
-                        return (in_array($a, ['aa','ab','af','ak','sq','am','ar','an','hy','as','av','ae','ay','az','ba','bm','eu','be','bn','bh','bi','bo','bs','br','bg','my','ca','cs','ch','ce','zh','cu','cv','kw','co','cr','cy','cs','da','de','dv','nl','dz','el','en','eo','et','eu','ee','fo','fa','fj','fi','fr','fr','fy','ff','ka','de','gd','ga','gl','gv','el','gn','gu','ht','ha','he','hz','hi','ho','hr','hu','hy','ig','is','io','ii','iu','ie','ia','id','ik','is','it','jv','ja','kl','kn','ks','ka','kr','kk','km','ki','rw','ky','kv','kg','ko','kj','ku','lo','la','lv','li','ln','lt','lb','lu','lg','mk','mh','ml','mi','mr','ms','mk','mg','mt','mn','mi','ms','my','na','nv','nr','nd','ng','ne','nl','nn','nb','no','ny','oc','oj','or','om','os','pa','fa','pi','pl','pt','ps','qu','rm','ro','ro','rn','ru','sg','sa','si','sk','sk','sl','se','sm','sn','sd','so','st','es','sq','sc','sr','ss','su','sw','sv','ty','ta','tt','te','tg','tl','th','bo','ti','to','tn','ts','tk','tr','tw','ug','uk','ur','uz','ve','vi','vo','cy','wa','wo','xh','yi','yo','za','zh','zu']));
-                    }                    
+                        $countries = ['AF','AX','AL','DZ','AS','AD','AO','AI','AQ','AG','AR','AM','AW','AU','AT','AZ','BS','BH','BD','BB','BY','BE','BZ','BJ','BM','BT','BO','BA','BW','BV','BR','IO','BN','BG','BF','BI','KH','CM','CA','CV','KY','CF','TD','CL','CN','CX','CC','CO','KM','CG','CD','CK','CR','CI','HR','CU','CY','CZ','DK','DJ','DM','DO','EC','EG','SV','GQ','ER','EE','ET','FK','FO','FJ','FI','FR','GF','PF','TF','GA','GM','GE','DE','GH','GI','GR','GL','GD','GP','GU','GT','GG','GN','GW','GY','HT','HM','VA','HN','HK','HU','IS','IN','ID','IR','IQ','IE','IM','IL','IT','JM','JP','JE','JO','KZ','KE','KI','KR','KW','KG','LA','LV','LB','LS','LR','LY','LI','LT','LU','MO','MK','MG','MW','MY','MV','ML','MT','MH','MQ','MR','MU','YT','MX','FM','MD','MC','MN','ME','MS','MA','MZ','MM','NA','NR','NP','NL','AN','NC','NZ','NI','NE','NG','NU','NF','MP','NO','OM','PK','PW','PS','PA','PG','PY','PE','PH','PN','PL','PT','PR','QA','RE','RO','RU','RW','BL','SH','KN','LC','MF','PM','VC','WS','SM','ST','SA','SN','RS','SC','SL','SG','SK','SI','SB','SO','ZA','GS','ES','LK','SD','SR','SJ','SZ','SE','CH','SY','TW','TJ','TZ','TH','TL','TG','TK','TO','TT','TN','TR','TM','TC','TV','UG','UA','AE','GB','US','UM','UY','UZ','VU','VE','VN','VG','VI','WF','EH','YE','ZM','ZW'];
+                        $langs = ['aa','ab','af','ak','sq','am','ar','an','hy','as','av','ae','ay','az','ba','bm','eu','be','bn','bh','bi','bo','bs','br','bg','my','ca','cs','ch','ce','zh','cu','cv','kw','co','cr','cy','cs','da','de','dv','nl','dz','el','en','eo','et','eu','ee','fo','fa','fj','fi','fr','fr','fy','ff','ka','de','gd','ga','gl','gv','el','gn','gu','ht','ha','he','hz','hi','ho','hr','hu','hy','ig','is','io','ii','iu','ie','ia','id','ik','is','it','jv','ja','kl','kn','ks','ka','kr','kk','km','ki','rw','ky','kv','kg','ko','kj','ku','lo','la','lv','li','ln','lt','lb','lu','lg','mk','mh','ml','mi','mr','ms','mk','mg','mt','mn','mi','ms','my','na','nv','nr','nd','ng','ne','nl','nn','nb','no','ny','oc','oj','or','om','os','pa','fa','pi','pl','pt','ps','qu','rm','ro','ro','rn','ru','sg','sa','si','sk','sk','sl','se','sm','sn','sd','so','st','es','sq','sc','sr','ss','su','sw','sv','ty','ta','tt','te','tg','tl','th','bo','ti','to','tn','ts','tk','tr','tw','ug','uk','ur','uz','ve','vi','vo','cy','wa','wo','xh','yi','yo','za','zh','zu'];
+                        if(strpos($a, '-') > 0) {
+                            $parts = explode('-', $a);
+                            return (in_array($parts[0], $langs)) && (in_array($parts[1], $countries));
+                        }
+                        return (in_array($a, $langs));
+                    }
                 ];
-            case 'language/iso-639:3': 
+            case 'language/iso-639:3':
                 return [
-                    'kind'  => 'function', 
+                    'kind'  => 'function',
                     'rule'  => function($a, $o) {
                         return (in_array($a, ['aar','abk','ace','ach','ada','ady','afa','afh','afr','ain','aka','akk','alb','sqi','ale','alg','alt','amh','ang','anp','apa','ara','arc','arg','arm','hye','arn','arp','art','arw','asm','ast','ath','aus','ava','ave','awa','aym','aze','bad','bai','bak','bal','bam','ban','baq','eus','bas','bat','bej','bel','bem','ben','ber','bho','bih','bik','bin','bis','bla','bnt','tib','bod','bos','bra','bre','btk','bua','bug','bul','bur','mya','byn','cad','cai','car','cat','cau','ceb','cel','cze','ces','cha','chb','che','chg','chi','zho','chk','chm','chn','cho','chp','chr','chu','chv','chy','cmc','cnr','cop','cor','cos','cpe','cpf','cpp','cre','crh','crp','csb','cus','wel','cym','cze','ces','dak','dan','dar','day','del','den','ger','deu','dgr','din','div','doi','dra','dsb','dua','dum','dut','nld','dyu','dzo','efi','egy','eka','gre','ell','elx','eng','enm','epo','est','baq','eus','ewe','ewo','fan','fao','per','fas','fat','fij','fil','fin','fiu','fon','fre','fra','fre','fra','frm','fro','frr','frs','fry','ful','fur','gaa','gay','gba','gem','geo','kat','ger','deu','gez','gil','gla','gle','glg','glv','gmh','goh','gon','gor','got','grb','grc','gre','ell','grn','gsw','guj','gwi','hai','hat','hau','haw','heb','her','hil','him','hin','hit','hmn','hmo','hrv','hsb','hun','hup','arm','hye','iba','ibo','ice','isl','ido','iii','ijo','iku','ile','ilo','ina','inc','ind','ine','inh','ipk','ira','iro','ice','isl','ita','jav','jbo','jpn','jpr','jrb','kaa','kab','kac','kal','kam','kan','kar','kas','geo','kat','kau','kaw','kaz','kbd','kha','khi','khm','kho','kik','kin','kir','kmb','kok','kom','kon','kor','kos','kpe','krc','krl','kro','kru','kua','kum','kur','kut','lad','lah','lam','lao','lat','lav','lez','lim','lin','lit','lol','loz','ltz','lua','lub','lug','lui','lun','luo','lus','mac','mkd','mad','mag','mah','mai','mak','mal','man','mao','mri','map','mar','mas','may','msa','mdf','mdr','men','mga','mic','min','mis','mac','mkd','mkh','mlg','mlt','mnc','mni','mno','moh','mon','mos','mao','mri','may','msa','mul','mun','mus','mwl','mwr','bur','mya','myn','myv','nah','nai','nap','nau','nav','nbl','nde','ndo','nds','nep','new','nia','nic','niu','dut','nld','nno','nob','nog','non','nor','nqo','nso','nub','nwc','nya','nym','nyn','nyo','nzi','oci','oji','ori','orm','osa','oss','ota','oto','paa','pag','pal','pam','pan','pap','pau','peo','per','fas','phi','phn','pli','pol','pon','por','pra','pro','pus','qaa','que','raj','rap','rar','roa','roh','rom','rum','ron','rum','ron','run','rup','rus','sad','sag','sah','sai','sal','sam','san','sas','sat','scn','sco','sel','sem','sga','sgn','shn','sid','sin','sio','sit','sla','slo','slk','slo','slk','slv','sma','sme','smi','smj','smn','smo','sms','sna','snd','snk','sog','som','son','sot','spa','alb','sqi','srd','srn','srp','srr','ssa','ssw','suk','sun','sus','sux','swa','swe','syc','syr','tah','tai','tam','tat','tel','tem','ter','tet','tgk','tgl','tha','tib','bod','tig','tir','tiv','tkl','tlh','tli','tmh','tog','ton','tpi','tsi','tsn','tso','tuk','tum','tup','tur','tut','tvl','twi','tyv','udm','uga','uig','ukr','umb','und','urd','uzb','vai','ven','vie','vol','vot','wak','wal','war','was','wel','cym','wen','wln','wol','xal','xho','yao','yap','yid','yor','ypk','zap','zbl','zen','zgh','zha','chi','zho','znd','zul','zun','zxx','zza']));
                     }
@@ -33,13 +47,13 @@ class DataValidator extends Service {
             case 'url/mailto':
             case 'uri/url:mailto':
                 return [
-                    'kind'  => 'function', 
+                    'kind'  => 'function',
                     'rule'  => function($a, $o) {return (bool) (preg_match('/^([_a-z0-9-]+)(\.[_a-z0-9+-]+)*@([a-z0-9-]+)(\.[a-z0-9-]+)*(\.[a-z]{2,13})$/', $a));}
                 ];
-            case 'currency/iso-4217': 
+            case 'currency/iso-4217':
             case 'currency/iso-4217:alpha':
                 return [
-                    'kind'  => 'function', 
+                    'kind'  => 'function',
                     'rule'  => function($a, $o) {
                         return (in_array($a, ['ADF','ADP','AED','AFA','AFN','ALL','AMD','ANG','AOA','AOK','AON','AOR','ARP','ARS','ATS','AUD','AWG','AZM','AZN','BAM','BBD','BDT','BEF','BGL','BGN','BHD','BIF','BMD','BND','BOB','BOP','BOV','BRL','BRR','BSD','BTN','BWP','BYB','BYR','BYN','BZD','CAD','CDF','CHE','CHF','CHW','CLF','CLP','CNY','COP','COU','CRC','CSD','CSK','CUC','CUP','CVE','CYP','CZK','DEM','DJF','DKK','DOP','DZD','ECS','ECV','EEK','EGP','ERN','ESP','ETB','EUR','FIM','FJD','FKP','FRF','GBP','GEL','GHS','GIP','GMD','GNF','GRD','GTQ','GWP','GYD','HKD','HNL','HRK','HTG','HUF','IDR','IEP','ILS','INR','IQD','IRR','ISK','ITL','JMD','JOD','JPY','KES','KGS','KHR','KMF','KPW','KRW','KZT','KWD','KYD','LAK','LBP','LKR','LRD','LSL','LTL','LUF','LVL','LVR','LYD','MAD','MDL','MGA','MGF','MKD','MMK','MNT','MOP','MRO','MRU','MTL','MUR','MVR','MWK','MXN','MXV','MYR','MZE','MZM','MZN','NAD','NGN','NHF','NIC','NIO','NLG','NOK','NPR','NZD','OMR','PAB','PEN','PES','PGK','PHP','PKR','PLN','PLZ','PTE','PYG','QAR','ROL','RON','RSD','RUB','RWF','SAR','SBD','SCR','SDD','SDG','SDP','SEK','SGD','SHP','SIT','SKK','SLL','SML','SOS','SRD','SSP','STD','SUB','SUR','SVC','SYP','SZL','THB','TJS','TMM','TMT','TND','TOP','TPE','TRL','TRY','TTD','TWD','TZS','UAH','UGX','USD','USN','USS','UYU','UYW','UZS','VAL','VEB','VEF','VES','VND','VUV','WST','XAF','XAG','XAU','XBA','XBB','XBC','XBD','XCD','XDR','XEU','XFO','XFU','XOF','XPD','XPF','XPT','XSU','XUA','YER','YUD','YUM','ZAR','ZMK','ZWD','ZWL','ZWR']));
                     }
@@ -55,12 +69,12 @@ class DataValidator extends Service {
             case 'password/NIST':
                 return [
                     'description' => 'NIST compliant password (min. 8 chars, 1 of @#$, 1 numeric digit, 1 uppercase, 1 lowercase).',
-                    'kind'  => 'function', 
+                    'kind'  => 'function',
                     'rule'  => function($a, $o) {return (bool) (preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$#])[A-Za-z\d@$#]{8,}$/', $a));}
-                ];                
+                ];
         }
         return [
-            'kind'  => 'function', 
+            'kind'  => 'function',
             'rule'  => function($a, $o) { return true; }
         ];
     }
@@ -75,14 +89,14 @@ class DataValidator extends Service {
     protected function __construct(/* no dependency */) {
         // initial configuration
     }
-    
-    /**  
+
+    /**
      * Tells if given $value comply with related $constraints set.
      *
      * Accepted elementary types are: 'boolean' (or 'bool'), 'integer' (or 'int'), 'double' (or 'float'), 'string', 'array', 'file'
      * 'file' is a pseudo type whic covers PHP file structure from multipart/form-data, base64 encoded binary value
      *
-     * Constraints is an array holding constraints description specific to the given value 
+     * Constraints is an array holding constraints description specific to the given value
      * it is an array of validation rules, each rule consist of a kind
      * - type (boolean, integer, double, string, date, array)
      * - 'min', 'max', 'in', 'not in'
@@ -93,15 +107,15 @@ class DataValidator extends Service {
      *
      * examples:
      * [ ['kind' => 'function', 'rule' => function($a) {return !($a%2);}],
-     *   ['kind' => 'min', 'rule' => 0 ],      
-     *   ['kind' => 'in', 'rule' => [1, 2, 3] ]     
+     *   ['kind' => 'min', 'rule' => 0 ],
+     *   ['kind' => 'in', 'rule' => [1, 2, 3] ]
      * ]
      *
      */
-    public function validate($value, $constraints) {        
+    public function validate($value, $constraints) {
         if(!is_array($constraints) || empty($constraints)) return true;
         foreach($constraints as $id => $constraint) {
-            // ignore empty constraints            
+            // ignore empty constraints
             if(!isset($constraint['kind']) || !isset($constraint['rule'])) {
                 continue;
             }
@@ -122,7 +136,7 @@ class DataValidator extends Service {
                     if(!in_array(gettype($value), ['string', 'array'])) return false;
                 }
                 // dates are handled as Unix timestamps
-                else if($constraint['rule'] == 'date' || $constraint['rule'] == 'datetime') {                    
+                else if($constraint['rule'] == 'date' || $constraint['rule'] == 'datetime') {
                     if(!gettype($value) == 'integer') return false;
                 }
                 else if(gettype($value) != $constraint['rule']) return false;
@@ -133,8 +147,8 @@ class DataValidator extends Service {
                     throw new \Exception("Invalid pattern {$constraint['rule']}", QN_ERROR_INVALID_CONFIG);
                 }
                 if(!preg_match($constraint['rule'], $value)) return false;
-                break;                
-            case 'function':            
+                break;
+            case 'function':
                 if(!is_callable($constraint['rule'])) {
                     throw new \Exception("Unknown function {$constraint['rule']}", QN_ERROR_INVALID_CONFIG);
                 }
@@ -147,12 +161,12 @@ class DataValidator extends Service {
                 switch(gettype($value)) {
                 case 'string':
                     if(strlen($value) < $constraint['rule']) return false;
-                    break;                        
+                    break;
                 case 'integer':
-                case 'double':                    
+                case 'double':
                     if($value < $constraint['rule']) return false;
                     break;
-                case 'array': 
+                case 'array':
                     if(count($value) < $constraint['rule']) return false;
                     break;
                 default:
@@ -167,12 +181,12 @@ class DataValidator extends Service {
                 switch(gettype($value)) {
                 case 'string':
                     if(strlen($value) > $constraint['rule']) return false;
-                    break;                        
+                    break;
                 case 'integer':
-                case 'double':                    
+                case 'double':
                     if($value > $constraint['rule']) return false;
                     break;
-                case 'array': 
+                case 'array':
                     if(count($value) > $constraint['rule']) return false;
                     break;
                 default:
@@ -181,11 +195,11 @@ class DataValidator extends Service {
                 }
                 break;
             case 'in':
-            case 'not in':            
+            case 'not in':
                 if(!is_array($constraint['rule'])) {
                     // warning : 'in' and 'not in' constraint has to be array
                     // try to force conversion to array
-                    $constraint['rule'] = [$constraint['rule']];                    
+                    $constraint['rule'] = [$constraint['rule']];
                 }
                 $type = gettype($value);
                 if($type == 'string') {
@@ -210,9 +224,9 @@ class DataValidator extends Service {
                             // error : while checking a float/double 'in' constraint has to hold float values
                             unset($constraint['rule'][$index]);
                         }
-                    }                
+                    }
                 }
-                else {                
+                else {
                     // error : unhandled value type for contraint 'max'
                     continue 2;
                 }
@@ -223,7 +237,7 @@ class DataValidator extends Service {
                 break;
             default:
                 // warning : unhandled constraint type {$constraint['kind']}
-                break;                        
+                break;
             }
         }
         return true;
