aca866136059a7951a8b52d69003bccc42019c14
unknown <cedricfrancoys@gmail.com>
2021-10-19 17:48:47 +0200

    added rules for fixed lenght numbers

--- lib/equal/data/DataValidator.class.php
+++ lib/equal/data/DataValidator.class.php
@@ -58,6 +58,106 @@ class DataValidator extends Service {
                         return (in_array($a, ['ADF','ADP','AED','AFA','AFN','ALL','AMD','ANG','AOA','AOK','AON','AOR','ARP','ARS','ATS','AUD','AWG','AZM','AZN','BAM','BBD','BDT','BEF','BGL','BGN','BHD','BIF','BMD','BND','BOB','BOP','BOV','BRL','BRR','BSD','BTN','BWP','BYB','BYR','BYN','BZD','CAD','CDF','CHE','CHF','CHW','CLF','CLP','CNY','COP','COU','CRC','CSD','CSK','CUC','CUP','CVE','CYP','CZK','DEM','DJF','DKK','DOP','DZD','ECS','ECV','EEK','EGP','ERN','ESP','ETB','EUR','FIM','FJD','FKP','FRF','GBP','GEL','GHS','GIP','GMD','GNF','GRD','GTQ','GWP','GYD','HKD','HNL','HRK','HTG','HUF','IDR','IEP','ILS','INR','IQD','IRR','ISK','ITL','JMD','JOD','JPY','KES','KGS','KHR','KMF','KPW','KRW','KZT','KWD','KYD','LAK','LBP','LKR','LRD','LSL','LTL','LUF','LVL','LVR','LYD','MAD','MDL','MGA','MGF','MKD','MMK','MNT','MOP','MRO','MRU','MTL','MUR','MVR','MWK','MXN','MXV','MYR','MZE','MZM','MZN','NAD','NGN','NHF','NIC','NIO','NLG','NOK','NPR','NZD','OMR','PAB','PEN','PES','PGK','PHP','PKR','PLN','PLZ','PTE','PYG','QAR','ROL','RON','RSD','RUB','RWF','SAR','SBD','SCR','SDD','SDG','SDP','SEK','SGD','SHP','SIT','SKK','SLL','SML','SOS','SRD','SSP','STD','SUB','SUR','SVC','SYP','SZL','THB','TJS','TMM','TMT','TND','TOP','TPE','TRL','TRY','TTD','TWD','TZS','UAH','UGX','USD','USN','USS','UYU','UYW','UZS','VAL','VEB','VEF','VES','VND','VUV','WST','XAF','XAG','XAU','XBA','XBB','XBC','XBD','XCD','XDR','XEU','XFO','XFU','XOF','XPD','XPF','XPT','XSU','XUA','YER','YUD','YUM','ZAR','ZMK','ZWD','ZWL','ZWR']));
                     }
                 ];
+            case 'numeric/decimal:1':
+                return [
+                    'kind'  => 'function',
+                    'rule'  => function($a, $o) { return (preg_match('/^[0-9]{1}$/', (string) $a));}
+                ];
+            case 'numeric/decimal:2':
+                return [
+                    'kind'  => 'function',
+                    'rule'  => function($a, $o) { return (preg_match('/^[0-9]{2}$/', (string) $a));}
+                ];
+            case 'numeric/decimal:3':
+                return [
+                    'kind'  => 'function',
+                    'rule'  => function($a, $o) { return (preg_match('/^[0-9]{3}$/', (string) $a));}
+                ];
+            case 'numeric/decimal:4':
+                return [
+                    'kind'  => 'function',
+                    'rule'  => function($a, $o) { return (preg_match('/^[0-9]{4}$/', (string) $a));}
+                ];
+            case 'numeric/decimal:5':
+                return [
+                    'kind'  => 'function',
+                    'rule'  => function($a, $o) { return (preg_match('/^[0-9]{5}$/', (string) $a));}
+                ];
+            case 'numeric/decimal:6':
+                return [
+                    'kind'  => 'function',
+                    'rule'  => function($a, $o) { return (preg_match('/^[0-9]{6}$/', (string) $a));}
+                ];
+            case 'numeric/decimal:7':
+                return [
+                    'kind'  => 'function',
+                    'rule'  => function($a, $o) { return (preg_match('/^[0-9]{7}$/', (string) $a));}
+                ];
+            case 'numeric/decimal:8':
+                return [
+                    'kind'  => 'function',
+                    'rule'  => function($a, $o) { return (preg_match('/^[0-9]{8}$/', (string) $a));}
+                ];
+            case 'numeric/decimal:9':
+                return [
+                    'kind'  => 'function',
+                    'rule'  => function($a, $o) { return (preg_match('/^[0-9]{9}$/', (string) $a));}
+                ];
+                break;
+            case 'numeric/hexadecimal:1':
+                return [
+                    'kind'  => 'function',
+                    'rule'  => function($a, $o) { return (preg_match('/^[0-9A-F]{1}$/', (string) $a)); }
+                ];
+                break;
+            case 'numeric/hexadecimal:2':
+                return [
+                    'kind'  => 'function',
+                    'rule'  => function($a, $o) { return (preg_match('/^[0-9A-F]{2}$/', (string) $a)); }
+                ];
+                break;
+            case 'numeric/hexadecimal:3':
+                return [
+                    'kind'  => 'function',
+                    'rule'  => function($a, $o) { return (preg_match('/^[0-9A-F]{3}$/', (string) $a)); }
+                ];
+                break;
+            case 'numeric/hexadecimal:4':
+                return [
+                    'kind'  => 'function',
+                    'rule'  => function($a, $o) { return (preg_match('/^[0-9A-F]{4}$/', (string) $a)); }
+                ];
+                break;
+            case 'numeric/hexadecimal:5':
+                return [
+                    'kind'  => 'function',
+                    'rule'  => function($a, $o) { return (preg_match('/^[0-9A-F]{5}$/', (string) $a)); }
+                ];
+                break;
+            case 'numeric/hexadecimal:6':
+                return [
+                    'kind'  => 'function',
+                    'rule'  => function($a, $o) { return (preg_match('/^[0-9A-F]{6}$/', (string) $a)); }
+                ];
+                break;
+            case 'numeric/hexadecimal:7':
+                return [
+                    'kind'  => 'function',
+                    'rule'  => function($a, $o) { return (preg_match('/^[0-9A-F]{7}$/', (string) $a)); }
+                ];
+                break;
+            case 'numeric/hexadecimal:8':
+                return [
+                    'kind'  => 'function',
+                    'rule'  => function($a, $o) { return (preg_match('/^[0-9A-F]{8}$/', (string) $a)); }
+                ];
+                break;
+            case 'numeric/hexadecimal:9':
+                return [
+                    'kind'  => 'function',
+                    'rule'  => function($a, $o) { return (preg_match('/^[0-9A-F]{9}$/', (string) $a)); }
+                ];
+                break;
             case 'password':
                 return [
                     'description' => 'Password having a length of 8 chars minimum.',
