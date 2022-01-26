b701fc9322aaf5dd6678a2818534bfd72fe2a313
unknown <cedricfrancoys@gmail.com>
2022-01-26 14:24:45 +0100

    added support for setting choices

---
+++ packages/core/classes/SettingChoice.class.php
@@ -0,0 +1,39 @@
+<?php
+/*
+    This file is part of the eQual framework <http://www.github.com/cedricfrancoys/equal>
+    Some Rights Reserved, Cedric Francoys, 2010-2021
+    Licensed under GNU GPL 3 license <http://www.gnu.org/licenses/>
+*/
+namespace core;
+
+use equal\orm\Model;
+
+class SettingChoice extends Model {
+
+    public static function getName() {
+        return 'Setting choice';
+    }
+
+    public static function getDescription() {
+        return "Assignable choice for a given setting.";
+    }
+
+    public static function getColumns() {
+        return [
+
+            'setting_id' => [
+                'type'              => 'many2one',
+                'foreign_object'    => 'core\Setting',
+                'description'       => 'Setting the choice relates to.',
+                'required'          => true
+            ],
+
+            'value' => [
+                'type'              => 'binary',
+                'description'       => 'JSON value of the parameter (type depends on the setting).'
+            ]
+
+        ];
+    }
+ 
+}
