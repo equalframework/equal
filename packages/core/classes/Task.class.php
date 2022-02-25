4e32db27e3d4d0906b9b3269e8ee48a7de4ecd26
unknown <cedricfrancoys@gmail.com>
2022-02-25 15:32:50 +0100

    updates

--- packages/core/classes/Task.class.php
+++ packages/core/classes/Task.class.php
@@ -18,19 +18,66 @@ class Task extends Model {
             'name' => [
                 'type'              => 'string',
                 'description'       => 'Name of the task, as set at creation.',
-                'default'           => ''
-            ],
-            'moment' => [
-                'type'              => 'datetime',
                 'required'          => true
             ],
+
+            'moment' => [
+                'type'              => 'datetime',
+                'required'          => true,
+                'description'       => 'Moment at which the task should be run for the next time.'
+            ],
+
+            'is_active'          => [
+                'type'              => 'boolean',
+                'default'           => true,
+                'description'       => 'Flag to mark the task as (temporarily) active or inactive.'
+            ],
+
+            'is_recurring'          => [
+                'type'              => 'boolean',
+                'default'           => false,
+                'description'       => 'Flag to mark the task as recurring (to be run more than once).'
+            ],
+
+            'repeat_axis' => [
+                'type'              => 'string',
+                'selection'         => [
+                    'minute', 'hour', 'day', 'week', 'month', 'year'
+                ],
+                'default'           => 'day',
+                'description'       => 'Basis for the repetition steps.',
+                'visible'           => ['is_recurring', '=', true]
+            ],
+
+            'repeat_step' => [
+                'type'              => 'integer',
+                'default'           => 1,
+                'description'       => 'Steps to wait before the tasks is run again.',
+                'visible'           => ['is_recurring', '=', true]
+            ],
+
             'controller' => [
                 'type'              => 'string',
                 'description'       => "Full notation of the action controller to invoke (ex. core_example_action)."
             ],
+
             'params' => [
                 'type'              => 'string',
-                'description'       => "un objet JSON object holding the parameters to relay to the controller."
+                'description'       => "JSON object holding the parameters to relay to the controller."
+            ]
+        ];
+    }
+
+    public static function getConstraints() {
+        return [
+            'repeat_step' =>  [
+                'invalid_step' => [
+                    'message'       => 'Step must be positive and max value depends on axis (for years, max is 10).',
+                    'function'      => function ($step, $values) {
+                        // #todo - improve (we need to make sure repeat_axis is in $values)
+                        return ($step >= 0 && $step < 60);
+                    }
+                ]
             ]
         ];
     }
