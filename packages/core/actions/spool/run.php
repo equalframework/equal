953f0d5075bb3afa987195eb2d9df6ecf1ac2e67
unknown <cedricfrancoys@gmail.com>
2022-02-25 15:32:23 +0100

    updates

--- packages/core/actions/spool/run.php
+++ packages/core/actions/spool/run.php
@@ -18,7 +18,7 @@ list($params, $providers) = announce([
 // initalise local vars with inputs
 list($om, $context, $spool, $auth) = [ $providers['orm'], $providers['context'], $providers['spool'], $providers['auth'] ];
 
-// todo : improve permissions
+// #todo : improve permissions checks
 
 if( $auth->userId() != ROOT_USER_ID) {
     throw new \Exception("spool_access_restricted", QN_ERROR_NOT_ALLOWED);
