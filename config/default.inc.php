1dc726f25cfe9120ec8720cdb7c22ad979038251
Cédric Françoys <cedricfrancoys@gmail.com>
2022-04-28 13:09:59 +0200

    moved debug mode to root config

--- config/default.inc.php
+++ config/default.inc.php
@@ -1,17 +1,17 @@
 <?php
-/** 
+/**
 * Main configuration file
 * Constants with name starting with QN_ are defined in eq.lib.php
 */
 namespace {
+    /*
+        Constants defined in this section are mandatory and cannot be modified in customs config.inc.php
+    */
+
     /**
-    * Constants defined in this section are mandatory and cannot be modified in customs config.inc.php
-    */    
-    
-    /** 
     * Add configuration in the global namespace: those cannot be changed in cascade config files.
     */
-    
+
     /**
      *
      * Possible values are: 'ORM' and 'JSON' (router.json)
@@ -23,8 +23,8 @@ namespace {
      * Routing configuration directory
      */
     define('ROUTING_CONFIG_DIR', QN_BASEDIR.'/config/routing');
-    
-    
+
+
     /**
      * Binary type storage mode
      *
@@ -36,75 +36,75 @@ namespace {
     /**
      * Binaries storage directory
      *
-     * Note: ensure http service has read/write permissions on this directory    
+     * Note: ensure http service has read/write permissions on this directory
      */
     define('FILE_STORAGE_DIR', QN_BASEDIR.'/bin');
 
 
     /**
-    * Default ACL
-    *
-    * If no ACL is defined (which is the case by default) for an object nor for its class, any user will be granted the permissions set below
-    */
+     * Default ACL
+     *
+     * If no ACL is defined (which is the case by default) for an object nor for its class, any user will be granted the permissions set below
+     */
     // Note: in order to allow a user to fully create objects, he must be granted QN_R_CREATE and QN_R_WRITE permissions
-    // Note: to set several rights at once, use the OR binary operator	
+    // Note: to set several rights at once, use the OR binary operator
     // define('DEFAULT_RIGHTS', QN_R_CREATE | QN_R_READ | QN_R_WRITE | QN_R_DELETE | QN_R_MANAGE);
-    
-    
+
+
     define('DEFAULT_RIGHTS', QN_R_CREATE | QN_R_READ | QN_R_DELETE | QN_R_WRITE);
     // define('DEFAULT_RIGHTS', 0);
 
 
     /**
-    * Access control level
-    */
+     * Access control level
+     */
     // By default, the control is done at the class level. It means that a user will be granted the same rights for every objects of a given class.
     // However, sometimes we must take the object id under account (for instance, if pages of a web site can have their own permissions)
     define('ACCESS_CONTROL_LEVEL', 'class');	// allowed values are 'class' or 'object'
-    
-    
+
+
     /**
-    * Language parameters
-    */
+     * Language parameters
+     */
     // The language in which the content must be displayed by default (ISO 639-1)
     define('DEFAULT_LANG', 'fr');
     define('GUEST_USER_LANG', 'fr');
-    
-    
-    /*
-    * EMAIL related parameters
-    */
+
+
+    /**
+     * EMAIL related parameters
+     */
     define('EMAIL_SMTP_HOST',               'SSL0.PROVIDER.NET');
     define('EMAIL_SMTP_PORT',               '587');
-    define('EMAIL_SMTP_ACCOUNT_DISPLAYNAME','Full Name');    
+    define('EMAIL_SMTP_ACCOUNT_DISPLAYNAME','Full Name');
     define('EMAIL_SMTP_ACCOUNT_USERNAME',   'email.as.username@provider.com');
     define('EMAIL_SMTP_ACCOUNT_PASSWORD',   'password');
     define('EMAIL_SMTP_ACCOUNT_EMAIL',	    'email.to.send.from@provider.com');
     define('EMAIL_SMTP_ABUSE_EMAIL',	    'abuse@example.com');
 
     /**
-    * Email spooler directory
-    */
+     * Email spooler directory
+     */
     // Note: ensure http service has read/write permissions on this directory
     define('EMAIL_SPOOL_DIR', QN_BASEDIR.'/spool');
 
-    
-    /**
-    * Database parameters
-    * note: most utilities need these parameters. 
+
+    /*
+        Database parameters
+        note: most utilities need these parameters.
     */
 
     /**
      * Database replication strategy
      *
-     * Possible values: 
+     * Possible values:
      * - 'NO': no replication
      * - 'MS' ('master-slave'): 2 servers; write operations are performed on both servers, read operations are performed on the master only
      * - 'MM' ('multi-master'): any number of servers; write operations are performed on all servers, read operations can be performed on any server
      */
-    define('DB_REPLICATION', 'MS'); 
-        
-    define('DB_DBMS',       'MYSQL');       
+    define('DB_REPLICATION', 'MS');
+
+    define('DB_DBMS',       'MYSQL');
     define('DB_CHARSET',    'UTF8');        // 'UTF8' in most cases
 
     define('DB_HOST',       '127.0.0.1');   // IP or fully qualified domain name (ex.: www.example.com)
@@ -112,55 +112,56 @@ namespace {
     define('DB_USER',       'root');        // this should be changed for security reasons
     define('DB_PASSWORD',   'test');        // this should be changed for security reasons
     define('DB_NAME',       'equal');       // the name of the DB that you've created or plan to use
-    
-    // additional DB server(s) can be configured as below
-    /* 
-    define('DB_1_HOST',       '127.0.0.1');  
-    define('DB_1_PORT',       '3306');       
-    define('DB_1_USER',       'root');       
-    define('DB_1_PASSWORD',   'test');       
-    define('DB_1_NAME',       'qinoa');      
 
-    define('DB_2_HOST',       '127.0.0.1');  
-    define('DB_2_PORT',       '3306');       
-    define('DB_2_USER',       'root');       
-    define('DB_2_PASSWORD',   'test');       
-    define('DB_2_NAME',       'qinoa');      
+    // additional DB server(s) can be configured as below
+    /*
+    define('DB_1_HOST',       '127.0.0.1');
+    define('DB_1_PORT',       '3306');
+    define('DB_1_USER',       'root');
+    define('DB_1_PASSWORD',   'test');
+    define('DB_1_NAME',       'qinoa');
+
+    define('DB_2_HOST',       '127.0.0.1');
+    define('DB_2_PORT',       '3306');
+    define('DB_2_USER',       'root');
+    define('DB_2_PASSWORD',   'test');
+    define('DB_2_NAME',       'qinoa');
 
     [...]
     */
 
 
     /**
-    * Default Package
-    */
+     * Default Package
+     */
     // Package we'll try to access if nothing is specified in the url (typically while accessing root folder)
     define('DEFAULT_PACKAGE', 'core');
-}
-namespace config {
-    /** 
-    * Constants defined in this section are mandatory but can be modified/re-defined in customs config.inc.php (i.e.: packages/[package_name]/config.inc.php)
-    *
-    */
 
-    // flag constant allowing to detect if config has been exported
-    define('EXPORT_FLAG', true);
-    
     /**
-    * Debugging
-    */	
+     * Debugging
+     */
     define('DEBUG_MODE', QN_DEBUG_PHP | QN_DEBUG_ORM | QN_DEBUG_SQL | QN_DEBUG_APP);
     // define('DEBUG_MODE', 0);
 
+}
+namespace config {
     /**
-    * List of public objects 
-    */
+     * Constants defined in this section are mandatory but can be modified/re-defined in customs config.inc.php (i.e.: packages/[package_name]/config.inc.php)
+     *
+     */
+
+    // flag constant allowing to detect if config has been exported
+    define('EXPORT_FLAG', true);
+
+    /**
+     * List of public objects
+     */
     // array of classes involved in right management and SPAM protection mechanism
     define ("PUBLIC_OBJECTS", serialize (array ('icway\Comment')));
-    
+
     /**
-    * File transfer parameters
-    */
+     * File transfer parameters
+     */
     // maximum authorized size for file upload (in octet)
     // keep in mind that this parameter does not override the PHP 'upload_max_filesize' and 'post_max_size' directives.
     // So it will not be effective if set higher than those.
@@ -216,7 +217,7 @@ namespace config {
 
     // define('HTTP_REDIRECT_404', '404.html');
     // define('HTTP_REDIRECT_500', '500.html');
-    
+
 // todo : generate a random key during install process
     define('AUTH_SECRET_KEY', 'my_secret_key');
 
@@ -227,6 +228,6 @@ namespace config {
     // limit sending of auth token to HTTPS
     define('AUTH_TOKEN_HTTPS', false);
     // define('AUTH_TOKEN_HTTPS', true);
-    
+
     define('ROOT_APP_URL', 'http://localhost');
 }
