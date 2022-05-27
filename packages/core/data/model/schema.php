6cd43bd9e47214d206d79d6da8783562d7f6ec71
Cédric Françoys <cedricfrancoys@gmail.com>
2022-05-27 10:30:24 +0200

    updates

--- packages/core/data/model/schema.php
+++ packages/core/data/model/schema.php
@@ -9,7 +9,7 @@ list($params, $providers) = announce([
     'params'        => [
         'entity' =>  [
             'description'   => 'Full name (including namespace) of the class to return (e.g. \'core\\User\').',
-            'type'          => 'string', 
+            'type'          => 'string',
             'required'      => true
         ],
     ],
@@ -18,64 +18,92 @@ list($params, $providers) = announce([
         'charset'       => 'UTF-8',
         'accept-origin' => '*'
     ],
-    'providers'     => ['context', 'orm', 'adapt'] 
+    'providers'     => ['context', 'orm', 'adapt']
 ]);
 
 
 list($context, $orm, $adapt) = [$providers['context'], $providers['orm'], $providers['adapt']];
 
 
-$data = array();
+$data = [];
 
-$model = $orm->getModel($params['entity']);
-if(!is_object($model)) {
-    throw new Exception("unknown class '{$params['entity']}'", QN_ERROR_UNKNOWN_OBJECT);
-}
-// get the complete schema of the object (including special fields)
-$schema = $model->getSchema();
+// handle controller entities
+$parts = explode('\\', $params['entity']);
+$file = array_pop($parts);
+if(ctype_lower(substr($file, 0, 1))) {
+    $package = array_shift($parts);
+    $path = implode('/', $parts);
+    $operation = str_replace('\\', '_', $params['entity']);
 
-// retrieve parent class
-$data['parent'] = get_parent_class($model);
-
-// retrieve root class (before Model)
-$root = $data['parent'];
-while($root != 'equal\orm\Model') {
-    $prev_parent = get_parent_class($root);
-    if($prev_parent == 'equal\orm\Model') {
-        break;
+    if(file_exists(QN_BASEDIR."/packages/{$package}/actions/{$path}/{$file}.php")) {
+        $result = eQual::run('do', $operation, ['announce' => true]);
+        $data = [
+            'fields' => $result['announcement']['params']
+        ];
     }
-    $root = $prev_parent;
+    else if(file_exists(QN_BASEDIR."/packages/{$package}/data/{$path}/{$file}.php")) {
+        $result = eQual::run('get', $operation, ['announce' => true]);
+        $data = [
+            'fields' => $result['announcement']['params']
+        ];
+    }
+    else {
+        throw new Exception("unknown_entity", QN_ERROR_UNKNOWN_OBJECT);
+    }
 }
-$data['root'] = $root;
 
-$data['table'] = $model->getTable();
+// entity did not relate to a controller
+if(!count($data)) {
+    $model = $orm->getModel($params['entity']);
+    if(!is_object($model)) {
+        throw new Exception("unknown_entity", QN_ERROR_UNKNOWN_OBJECT);
+    }
+    // get the complete schema of the object (including special fields)
+    $schema = $model->getSchema();
 
-$data['fields'] = $schema;
+    // retrieve parent class
+    $data['parent'] = get_parent_class($model);
 
-if(method_exists($model, 'getUnique')) {
-    $data['unique'] = $model->getUnique();
-}
-
-if(method_exists($model, 'getDefaults')) {
-    $defaults = $model->getDefaults();
-    foreach($defaults as $field => $default) {
-        if(is_callable($defaults[$field])) {
-            $default = call_user_func($defaults[$field], $orm);
+    // retrieve root class (before Model)
+    $root = $data['parent'];
+    while($root != 'equal\orm\Model') {
+        $prev_parent = get_parent_class($root);
+        if($prev_parent == 'equal\orm\Model') {
+            break;
         }
-        $type = $schema[$field]['type'];
-        $adapted = $adapt->adapt($default, $type, 'txt', 'php');
-        $data['fields'][$field]['default'] = $adapted;
+        $root = $prev_parent;
     }
+    $data['root'] = $root;
+
+    $data['table'] = $model->getTable();
+
+    $data['fields'] = $schema;
+
+    if(method_exists($model, 'getUnique')) {
+        $data['unique'] = $model->getUnique();
+    }
+
+    if(method_exists($model, 'getDefaults')) {
+        $defaults = $model->getDefaults();
+        foreach($defaults as $field => $default) {
+            if(is_callable($defaults[$field])) {
+                $default = call_user_func($defaults[$field], $orm);
+            }
+            $type = $schema[$field]['type'];
+            $adapted = $adapt->adapt($default, $type, 'txt', 'php');
+            $data['fields'][$field]['default'] = $adapted;
+        }
+    }
+
+
+    /*
+    if(method_exists($model, 'getConstraints')) {
+        $data['constraints'] = $model::getConstraints();
+    }
+    */
 }
 
 
-/*
-if(method_exists($model, 'getConstraints')) {
-    $data['constraints'] = $model::getConstraints();
-}
-*/
-
-   
 $context->httpResponse()
         ->body($data)
         ->send();
