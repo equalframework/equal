d34be8a9ee1d0dcfc33d423d065a020983e2d9cd
Cédric Françoys <cedricfrancoys@gmail.com>
2022-06-18 16:56:16 +0200

    added support for array as order

--- packages/core/data/model/export-pdf.php
+++ packages/core/data/model/export-pdf.php
@@ -122,6 +122,9 @@ if(in_array($params['controller'], ['model_collect', 'core_model_collect'])) {
     $start = (isset($params['params']['start']))?$params['params']['start']:0;
     $order = (isset($params['params']['order']))?$params['params']['order']:'id';
     $sort = (isset($params['params']['sort']))?$params['params']['sort']:'asc';
+    if(is_array($order)) {
+        $order = $order[0];
+    }
     $values = $params['entity']::search($params['domain'], ['sort' => [$order => $sort]])->shift($start)->limit($limit)->read($fields_to_read)->get();
 }
 else {
