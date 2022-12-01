6fc85f0b3284cc09694e58de998a1490ff0ccaae
Cédric Françoys <cedricfrancoys@gmail.com>
2022-12-01 19:07:43 +0100

    syntax & cascade updates

--- packages/core/classes/setting/SettingSection.class.php
+++ packages/core/classes/setting/SettingSection.class.php
@@ -1,3 +1,4 @@
+
 <?php
 /*
     This file is part of the eQual framework <http://www.github.com/cedricfrancoys/equal>
@@ -50,11 +51,10 @@ class SettingSection extends Model {
     }
 
     public static function onupdateCode($om, $ids, $values, $lang) {
-
-        $sections = $om->read(__CLASS__, $ids, ['settings_ids'], $lang);
+        $sections = $om->read(self::getType(), $ids, ['settings_ids'], $lang);
         if($sections > 0 && count($sections)) {
             foreach($sections as $oid => $odata) {
-                $om->update('core\setting\Setting', $odata['settings_ids'], ['name' => null], $lang);
+                $om->update(Setting::getType(), $odata['settings_ids'], ['name' => null, 'section' => null], $lang);
             }
         }
 
