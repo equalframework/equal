cbba91f8cc18bc0fe20e3af086abf242912e837b
Cédric Françoys <cedricfrancoys@gmail.com>
2023-04-19 21:57:36 +0200

    updated

--- packages/core/apps/settings/build.sh
+++ packages/core/apps/settings/build.sh
@@ -3,4 +3,5 @@ rm -rf .angular
 npm link sb-shared-lib
 ng build --configuration production --output-hashing none --base-href="//settings\\"
 touch manifest.json && rm -f web.app && cp manifest.json dist/symbiose/ && cd dist/symbiose && zip -r ../../web.app * && cd ../..
-cat web.app | md5sum | awk '{print $1}'
+cat web.app | md5sum | awk '{print $1}' > version
+cat version
