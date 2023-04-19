93e2c4bd6045ad9e5dc2783ba078161ff94aa79c
Cédric Françoys <cedricfrancoys@gmail.com>
2023-04-19 21:50:46 +0200

    made email sending asynchronous

--- packages/core/actions/user/signup.php
+++ packages/core/actions/user/signup.php
@@ -1,13 +1,7 @@
 <?php
-if(!file_exists(QN_BASEDIR.'/vendor/swiftmailer/swiftmailer/lib/swift_required.php')) {
-    throw new Exception("missing_dependency", QN_ERROR_INVALID_CONFIG);
-}
-require_once QN_BASEDIR.'/vendor/swiftmailer/swiftmailer/lib/swift_required.php';
-
-use \Swift_SmtpTransport as Swift_SmtpTransport;
-use \Swift_Message as Swift_Message;
-use \Swift_Mailer as Swift_Mailer;
 use equal\html\HtmlTemplate;
+use equal\email\Email;
+use core\Mail;
 use core\User;
 
 // announce script and fetch parameters values
@@ -29,7 +23,7 @@ list($params, $providers) = announce([
         'firstname' => [
             'description'   => 'User\'s firstname.',
             'type'          => 'string',
-            'required'      => true
+            'default'       => ''
         ],
         'lastname' => [
             'description'   => 'User\'s lastname.',
@@ -45,9 +39,17 @@ list($params, $providers) = announce([
             'description'   => 'Flag telling if we need to send a confirmation email.',
             'type'          => 'boolean',
             'default'       => true
+        ],
+        'resend' => [
+            'description'   => 'Previously sent message identifier to resend (must match credentials).',
+            'type'          => 'integer',
+            'default'       => 0
         ]
     ],
-    'constants'     => ['DEFAULT_LANG', 'AUTH_ACCESS_TOKEN_VALIDITY', 'EMAIL_SMTP_HOST', 'EMAIL_SMTP_PORT', 'EMAIL_SMTP_ACCOUNT_DISPLAYNAME', 'EMAIL_SMTP_ACCOUNT_USERNAME', 'EMAIL_SMTP_ACCOUNT_PASSWORD'],
+    'constants'     => ['DEFAULT_LANG', 'EMAIL_SMTP_HOST', 'EMAIL_SMTP_ACCOUNT_DISPLAYNAME'],
+    'access'        => [
+        'visibility'        => 'public'
+    ],
     'response'      => [
         'content-type'      => 'application/json',
         'charset'           => 'utf-8',
@@ -57,11 +59,10 @@ list($params, $providers) = announce([
 ]);
 
 
-// initalise local vars with inputs
+// initialize local vars with inputs
 list($om, $context, $auth) = [ $providers['orm'], $providers['context'], $providers['auth'] ];
 
 // cleanup provided email (as login): we strip heading and trailing spaces and remove recipient tag, if any
-// #memo - email might still be invalid (a validation check is made in User class)
 list($username, $domain) = explode('@', strtolower(trim($params['login'])));
 $username .= '+';
 $login = substr($username, 0, strpos($username, '+')).'@'.$domain;
@@ -71,91 +72,86 @@ list($password, $firstname, $lastname, $language, $send_confirm) = [
     $params['firstname'],
     $params['lastname'],
     $params['language'],
-    $params['send_confirm']                 // set to false only if user is registering through an authentified source (SSO)
+    // set to false only if user is registering through an authenticated source (SSO)
+    $params['send_confirm']
 ];
 
-// try to create a new user account
-$json = run('do', 'user_create', [
-        'login'         => $login,
-        'password'      => $password,
-        'firstname'     => $firstname,
-        'lastname'      => $lastname,
-        'language'      => $language
-    ]);
+// unique identifier of the Mail message
+$message_id = 0;
 
+// get root privileges
+$auth->su();
 
-// decode json into an array
-$data = json_decode($json, true);
+// check the existence of the user account
+$ids = $om->search(User::gettype(), [['login', '=', $login]]);
 
-// relay error if any
-if(isset($data['errors'])) {
-    foreach($data['errors'] as $name => $message) {
-        throw new Exception($message, qn_error_code($name));
+if($params['resend']) {
+    if(count($ids) <= 0) {
+        throw new Exception('invalid_request', QN_ERROR_INVALID_USER);
     }
+    $user_id = reset($ids);
+    $user = User::id($user_id)->read(['login', 'firstname', 'lastname'])->first(true);
+    $message_id = $params['resend'];
+    // if message is still in pool : abort
+    $send_confirm = !(Mail::isQueued($message_id));
+}
+else {
+    if(count($ids) > 0) {
+        throw new Exception('existing_user', QN_ERROR_INVALID_USER);
+    }
+    // #memo - email might still be invalid (a validation check is made in User class)
+    $user = User::create($params)
+        ->read(['id', 'login', 'firstname', 'lastname', 'language'])
+        ->adapt('json')
+        ->first(true);
 }
 
-// we received an array describing a User object
-$user = $data;
-// generate access_token
-$access_token = $auth->token($user['id'], constant('AUTH_ACCESS_TOKEN_VALIDITY'));
-
-
 if($send_confirm) {
-
     // we need the original password to generate confirmation code in the email
     $user['password'] = $password;
-
     // subject of the email should be defined in the template, as a <var> tag holding a 'title' attribute
     $subject = '';
-    // read template according to user prefered language
+    // read template according to user preferred language
     $file = "packages/core/i18n/{$user['language']}/mail_user_confirm.html";
     if(!($html = @file_get_contents($file))) {
         throw new Exception("missing_dependency", QN_ERROR_INVALID_CONFIG);
     }
     $template = new HtmlTemplate($html, [
-        'subject'		=>	function ($params, $attributes) use (&$subject) {
-                                $subject = $attributes['title'];
-                                return '';
-                            },
-        'username'		=>	function ($params, $attributes) {
-                                return $params['firstname'];
-                            },
-        'confirm_url'	=>	function ($params, $attributes) use($context) {
-                                $code = base64_encode($params['login'].':'.$params['password']);
-                                $uri = $context->getHttpRequest()->getUri();
-                                $url = $uri->getScheme().'://'.$uri->getAuthority();
-                                $url = $url."/?do=user_confirm&code={$code}";
-                                return "<a href=\"$url\">{$attributes['title']}</a>";
-                            },
-        'origin'        =>  function ($params, $attributes) {
-                                return constant('EMAIL_SMTP_ACCOUNT_DISPLAYNAME');
-                            }
-    ],
-    $user);
+            'subject'		=>	function ($params, $attributes) use (&$subject) {
+                                    $subject = $attributes['title'];
+                                    return '';
+                                },
+            'username'		=>	function ($params, $attributes) {
+                                    return $params['firstname'];
+                                },
+            'confirm_url'	=>	function ($params, $attributes) use ($context) {
+                                    $code = base64_encode($params['login'].':'.$params['password']);
+                                    $uri = $context->getHttpRequest()->getUri();
+                                    $url = $uri->getScheme().'://'.$uri->getAuthority();
+                                    $url = $url."/?do=user_confirm&code={$code}";
+                                    return "<a href=\"$url\">{$attributes['title']}</a>";
+                                },
+            'origin'        =>  function ($params, $attributes) {
+                                    return constant('EMAIL_SMTP_ACCOUNT_DISPLAYNAME');
+                                }
+        ],
+        $user);
 
     // parse template as html
     $body = $template->getHtml();
 
-
-    // send message
-    $transport = new Swift_SmtpTransport(constant('EMAIL_SMTP_HOST'), constant('EMAIL_SMTP_PORT') /*, 'ssl'*/);
-
-    $transport->setUsername(constant('EMAIL_SMTP_ACCOUNT_USERNAME'))
-              ->setPassword(constant('EMAIL_SMTP_ACCOUNT_PASSWORD'));
-
-    $message = new Swift_Message();
+    // create message
+    $message = new Email();
     $message->setTo($user['login'])
             ->setSubject($subject)
             ->setContentType("text/html")
-            ->setBody($body)
-            ->setFrom([constant('EMAIL_SMTP_ACCOUNT_USERNAME') => constant('EMAIL_SMTP_ACCOUNT_DISPLAYNAME')]);
+            ->setBody($body);
 
-    $mailer = new Swift_Mailer($transport);
-    $result = $mailer->send($message);
+    // queue message
+    $message_id = Mail::queue($message);
 }
 
 $context->httpResponse()
-        // store token in cookie
-        ->cookie('access_token', $access_token)
-        ->body(['access_token' => $access_token])
-        ->send();
+    ->status(200)
+    ->body(['message_id' => $message_id])
+    ->send();
