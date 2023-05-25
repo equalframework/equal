87842b443c4a922b53d410b18c652649b696ab6e
Cédric Françoys <cedricfrancoys@gmail.com>
2023-05-25 13:06:44 +0200

    added support for collection transition

---
+++ packages/core/actions/model/transition.php
@@ -0,0 +1,73 @@
+<?php
+/*
+    This file is part of the eQual framework <http://www.github.com/cedricfrancoys/equal>
+    Some Rights Reserved, Cedric Francoys, 2010-2021
+    Licensed under GNU LGPL 3 license <http://www.gnu.org/licenses/>
+*/
+list($params, $providers) = announce([
+    'description'   => "Returns a map with the descriptors of the installed apps (depending on initialized packages).",
+    'params'        => [
+        'entity' =>  [
+            'description'   => 'Full name (including namespace) of the class to look into (e.g. \'core\\User\').',
+            'type'          => 'string',
+            'required'      => true
+        ],
+        'id' =>  [
+            'description'   => 'Unique identifier of the object to remove.',
+            'type'          => 'integer',
+            'default'       => 0
+        ],
+        'ids' =>  [
+            'description'   => 'List of Unique identifiers of the objects to update.',
+            'type'          => 'array',
+            'default'       => []
+        ],
+        'transition' =>  [
+            'description'   => 'Name of the transition to attempt to apply on the objects.',
+            'type'          => 'string',
+            'required'      => true
+        ]
+    ],
+    'response'      => [
+        'content-type'  => 'application/json',
+        'charset'       => 'utf-8',
+        'accept-origin' => '*'
+    ],
+    'providers'     => ['context', 'orm']
+]);
+
+/**
+ * @var \equal\php\Context                  $context
+ * @var \equal\orm\ObjectManager            $orm
+ */
+list($context, $orm) = [$providers['context'], $providers['orm']];
+
+// retrieve target entity
+$entity = $orm->getModel($params['entity']);
+if(!$entity) {
+    throw new Exception("unknown_entity", QN_ERROR_INVALID_PARAM);
+}
+
+// sanitize ids
+if(empty($params['ids'])) {
+    if( !isset($params['id']) || $params['id'] <= 0 ) {
+        throw new Exception("object_invalid_id", QN_ERROR_INVALID_PARAM);
+    }
+    $params['ids'][] = $params['id'];
+}
+
+$res = $orm->transition($params['entity'], $params['ids'], $params['transition']);
+
+if(count($res)) {
+    foreach($res as $error_code => $error_descr) {
+        if(is_array($error_descr)) {
+            $error_descr = serialize($error_descr);
+        }
+        // send error using the same format as the announce method
+        throw new \Exception($error_descr, (int) $error_code);
+    }
+}
+
+$context->httpResponse()
+    ->status(204)
+    ->send();
