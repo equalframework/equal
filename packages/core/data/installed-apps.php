32bcc7ea859e869fe4f5539f3a1d05506895aa42
Cédric Françoys <cedricfrancoys@gmail.com>
2023-07-03 11:18:10 +0200

    added support for virtual apps

--- packages/core/data/installed-apps.php
+++ packages/core/data/installed-apps.php
@@ -6,8 +6,6 @@
 */
 list($params, $providers) = announce([
     'description'   => "Returns a map with the descriptors of the installed apps (depending on initialized packages).",
-    'params'        => [
-    ],
     'response'      => [
         'content-type'  => 'application/json',
         'charset'       => 'utf-8',
@@ -22,17 +20,50 @@ list($params, $providers) = announce([
  */
 list($context, $orm) = [$providers['context'], $providers['orm']];
 
-$manifests_map = [];
+$map_manifests = [];
 
 // search and read manifest.json inside App folders within /public
 foreach(glob("public/*", GLOB_ONLYDIR) as $app_path) {
     if(file_exists("$app_path/manifest.json")) {
         if($manifest = json_decode(file_get_contents("$app_path/manifest.json"), true)) {
-            $manifests_map[basename($app_path)] = $manifest;
+            $map_manifests[basename($app_path)] = $manifest;
+        }
+    }
+}
+
+// loop through initialized packages to include apps extending others
+if(file_exists("log/packages.json")) {
+    $packages = json_decode(file_get_contents("log/packages.json"), true);
+    foreach($packages as $package => $datetime) {
+        if(file_exists("packages/$package/manifest.json")) {
+            $manifest = json_decode(file_get_contents("packages/$package/manifest.json"), true);
+            if(!isset($manifest['apps'])) {
+                continue;
+            }
+            // handle apps using app descriptors
+            foreach($manifest['apps'] as $app) {
+                // discard string notation (refers to an app ID)
+                if(!is_array($app)) {
+                    continue;
+                }
+                // discard apps not extending another App
+                if(!isset($app['extends'])) {
+                    continue;
+                }
+                // discard apps extending a non-installed App
+                if(!file_exists("public/{$app['extends']}") || !is_dir("public/{$app['extends']}")) {
+                    continue;
+                }
+                $app['url'] = "/{$app['extends']}/#/$package";
+                // force app to be visible
+                $app['show_in_apps'] = true;
+                $map_manifests[$app['id']] = $app;
+
+            }
         }
     }
 }
 
 $context->httpResponse()
-    ->body($manifests_map)
+    ->body($map_manifests)
     ->send();
