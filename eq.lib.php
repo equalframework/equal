fd84ff55b3d1197af51b9827e932f63cc99be0b3
Cédric Françoys <cedricfrancoys@gmail.com>
2023-09-04 17:55:28 +0200

    added missing global declarations

--- eq.lib.php
+++ eq.lib.php
@@ -20,6 +20,9 @@
 *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 namespace {
+    global $last_context;
+    global $constants_schema;
+
     define('__EQ_LIB', true) or die('fatal error: __EQ_LIB already defined or cannot be defined');
 
     /**
