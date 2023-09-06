16887643d428844bda2e8bcb2174823cc86773d7
Cédric Françoys <cedricfrancoys@gmail.com>
2023-09-06 15:23:48 +0200

    updated

--- lib/equal/cron/Scheduler.class.php
+++ lib/equal/cron/Scheduler.class.php
@@ -29,35 +29,74 @@ class Scheduler extends Service {
      * At each call we check all active tasks and execute the ones having the `moment` field (timestamp) overdue.
      * For recurring tasks we update the moment field to the next time, according to repeat axis and repeat step.
      * Non-recurring tasks are deleted once they've been run.
+     * #memo - The Scheduler service always operates as root user.
      *
-     * #memo - Scheduler always operates as root user.
+     * @param   int[]   $tasks_ids    Optional array of identifiers of specific tasks to run.
+     *
      */
-    public function run() {
+    public function run($tasks_ids=[]) {
         $orm = $this->container->get('orm');
 
-        $tasks_ids = $orm->search('core\Task', ['is_active', '=', true], ['moment' => 'asc'], 0, 10);
+        $selected_tasks_ids = $tasks_ids;
+        $now = time();
 
-        if($tasks_ids > 0) {
-            $now = time();
-            $tasks = $orm->read('core\Task', $tasks_ids, ['id', 'moment', 'is_recurring', 'repeat_axis', 'repeat_step', 'controller', 'params']);
+        if(!count($tasks_ids)) {
+            // no specific task is requested, fetch all active tasks (limit to max 10 tasks per batch)
+            $selected_tasks_ids = $orm->search('core\Task', [['is_active', '=', true], ['moment', '<=', $now]], ['moment' => 'asc'], 0, 10);
+        }
+
+        if($selected_tasks_ids > 0 && count($selected_tasks_ids)) {
+            $tasks = $orm->read('core\Task', $selected_tasks_ids, ['id', 'moment', 'status', 'is_recurring', 'repeat_axis', 'repeat_step', 'controller', 'params']);
             foreach($tasks as $tid => $task) {
-                if($task['moment'] <= $now) {
-                    // run
+                // prevent concurrent execution
+                if($task['status'] != 'idle') {
+                    // trigger_error("PHP::Ignoring execution of task that is already running [{$task['id']}] - [{$task['controller']}]", QN_REPORT_INFO);
+                    continue;
+                }
+                // mark the task as running and update last_run
+                $orm->update('core\Task', $tid, ['status' => 'running', 'last_run' => $now]);
+                // #todo - add current PID for force stop (exec("kill -9 $pid")) / $context->getPid()
+                // if due time has passed or if specific tasks_ids are given, execute the task
+                if($task['moment'] <= $now || count($tasks_ids) > 0) {
+                    // if no specific tasks_ids are given, update each task
+                    if(!count($tasks_ids)) {
+                        // #memo - we must start by updating the task because some controllers might run for a duration longer than the remaining time before the next `run()` call
+                        if($task['is_recurring']) {
+                            $moment = $task['moment'];
+                            while($moment < $now) {
+                                $moment = strtotime("+{$task['repeat_step']} {$task['repeat_axis']}", $moment);
+                            }
+                            $orm->update('core\Task', $tid, ['moment' => $moment]);
+                        }
+                        else {
+                            // #todo - add support for keeping task and de-activating it instead of deleting it
+                            // @see after_execution
+                            $orm->delete('core\Task', $tid, true);
+                        }
+                    }
+                    list($status, $log) = ['', ''];
                     try {
                         $body = json_decode($task['params'], true);
-                        \eQual::run('do', $task['controller'], $body, true);
+                        // run the task
+                        $data = \eQual::run('do', $task['controller'], $body, true);
+                        $status = 'success';
+	                    $log = json_encode($data, JSON_PRETTY_PRINT);
                     }
                     catch(\Exception $e) {
                         // error occurred during execution
+                        // trigger_error("PHP::Error while running scheduled job [{$task['id']}]: ".$e->getMessage(), QN_REPORT_ERROR);
+                        $status = 'error';
+                        $msg = $e->getMessage();
+                        $data = @unserialize($msg);
+                        if(is_array($data)) {
+                            $data = json_encode($data, JSON_PRETTY_PRINT);
+                        }
+                        $log = ($data)?$data:$msg;
                     }
-                    // update task, if recurring
-                    if($task['is_recurring']) {
-                        $moment = strtotime("+{$task['repeat_step']} {$task['repeat_axis']}", $now);
-                        $orm->update('core\Task', $tid, ['moment' => $moment]);
-                    }
-                    else {
-                        $orm->remove('core\Task', $tid, true);
-                    }
+                    // create a new TaskLog holding result
+                    $orm->create('core\TaskLog', ['task_id' => $tid, 'status' => $status, 'log' => $log]);
+                    // mark the task as idle (so it can be executed again)
+                    $orm->update('core\Task', $tid, ['status' => 'idle']);
                 }
             }
         }
@@ -69,27 +108,25 @@ class Scheduler extends Service {
      *
      * @param   string    $name         Name of the task to schedule, to ease task identification.
      * @param   integer   $moment       Timestamp of the moment of the first execution.
-     * @param   string    $controller   Controller to invoker, with package notation.
+     * @param   string    $controller   Controller to invoke, with package notation.
      * @param   string    $params       JSON string holding the payload to relay to the controller.
      * @param   boolean   $recurring    Flag to mark the task as recurring.
      */
     public function schedule($name, $moment, $controller, $params, $recurring=false, $repeat_axis='day', $repeat_step='1') {
         $orm = $this->container->get('orm');
-        trigger_error("scheduling job", E_USER_WARNING);
+        // trigger_error("PHP::Scheduling job", QN_REPORT_INFO);
 
-        if($moment > time()) {
-            $orm->create('core\Task', [
-                'name'          => $name,
-                'moment'        => $moment,
-                'controller'    => $controller,
-                'params'        => json_encode($params),
-                'is_recurring'  => $recurring
-            ]);
-        }
+        return $orm->create('core\Task', [
+            'name'          => $name,
+            'moment'        => $moment,
+            'controller'    => $controller,
+            'params'        => json_encode($params),
+            'is_recurring'  => $recurring
+        ]);
     }
 
     /**
-     * Cancels (delete) a scheduled task.
+     * Cancels a scheduled task (by deleting it).
      * #memo - Scheduler always operates as root user.
      *
      * @param   string    $name         Name of the task to cancel.
@@ -98,8 +135,9 @@ class Scheduler extends Service {
         $orm = $this->container->get('orm');
         $tasks_ids = $orm->search('core\Task', ['name', '=', $name]);
         if($tasks_ids > 0 && count($tasks_ids)) {
-            $orm->remove('core\Task', $tasks_ids, true);
+            return $orm->remove('core\Task', $tasks_ids, true);
         }
+        return QN_ERROR_UNKNOWN_OBJECT;
     }
 
 }
