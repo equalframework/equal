59be8e163d25d218e613ab9b849c1c92c6f92142
Cédric Françoys <cedricfrancoys@gmail.com>
2023-09-28 13:38:09 +0200

    fixed minor issues

--- packages/core/actions/config/update-view.php
+++ packages/core/actions/config/update-view.php
@@ -1,6 +1,10 @@
 <?php
-
-list($params, $providers) = announce([
+/*
+    This file is part of the eQual framework <http://www.github.com/cedricfrancoys/equal>
+    Some Rights Reserved, Cedric Francoys, 2010-2021
+    Licensed under GNU LGPL 3 license <http://www.gnu.org/licenses/>
+*/
+list($params, $providers) = eQual::announce([
     'description'   => "save a representation of a view to a json file",
     'response'      => [
         'content-type'  => 'text/plain',
@@ -9,17 +13,17 @@ list($params, $providers) = announce([
     ],
     'params' => [
         'entity' => [
-            'description' => 'name of the entity',
+            'description' => 'Name of the entity.',
             'type' => 'string',
             'required' => true
         ],
-        'viewid' => [
-            'description' => 'id of the view',
+        'view_id' => [
+            'description' => 'Identifier of the view.',
             'type' => 'string',
             'required' => true
         ],
         'payload' =>  [
-            'description'   => 'view definition .',
+            'description'   => 'View definition (JSON).',
             'type'          => 'text',
             'required'      => true
         ]
@@ -32,9 +36,7 @@ list($context, $orm, $adapter) = [$providers['context'], $providers['orm'], $pro
 $parts = explode("\\",$params['entity']);
 
 $package = array_shift($parts);
-$entity = implode("/",$parts);
-
-$payload = $params['payload'];
+$entity = implode("/", $parts);
 
 // Checking if package exists
 if(!file_exists("packages/{$package}")) {
@@ -44,7 +46,7 @@ if(!file_exists("packages/{$package}/views")) {
     throw new Exception('missing_views_dir', QN_ERROR_INVALID_CONFIG);
 }
 
-$file = QN_BASEDIR."/packages/{$package}/views/{$entity}.{$params[viewid]}.json";
+$file = QN_BASEDIR."/packages/{$package}/views/{$entity}.{$params['view_id']}.json";
 
 // Check if the view exists
 if(!file_exists($file)){
@@ -52,20 +54,20 @@ if(!file_exists($file)){
 }
 
 // Checking if json is conform
-if( ($payload_decoded = json_decode($payload, true)) === null) {
+if( ($payload_decoded = json_decode($params['payload'], true)) === null) {
     throw new Exception("malformed_view_schema", QN_ERROR_INVALID_CONFIG);
 }
 
-$pretty_payload = json_encode($payload_decoded, JSON_PRETTY_PRINT);
+$json = json_encode($payload_decoded, JSON_PRETTY_PRINT);
 
-$IOERROR = file_put_contents($file,$pretty_payload);
+$result = file_put_contents($file, $json);
 
-if(!$IOERROR){
-    throw new Exception("IOError : check file rights", QN_ERROR_INVALID_CONFIG);
+if(!$result){
+    throw new Exception("file_update_denied", QN_ERROR_INVALID_CONFIG);
 }
 
 $result = file_get_contents($file);
 
 $context->httpResponse()
-        ->body("{$result}")
+        ->status(204)
         ->send();
