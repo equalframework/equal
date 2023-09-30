266d9ae191ec63d442877b70aa0afecfda7cc417
Cédric Françoys <cedricfrancoys@gmail.com>
2023-09-30 13:52:31 +0200

    added

---
+++ packages/core/apps/discord.php
@@ -0,0 +1,13 @@
+<?php
+/*
+    This file is part of the eQual framework <http://www.github.com/cedricfrancoys/equal>
+    Some Rights Reserved, Cedric Francoys, 2010-2021
+    Licensed under GNU LGPL 3 license <http://www.gnu.org/licenses/>
+*/
+list($params, $providers) = eQual::announce([
+    'description'   => 'Redirect to eQual Discord server.',
+    'params'        => [],
+    'response'      => [
+        'location'      => 'https://discord.gg/BNCPYxD9kk'
+    ]
+]);
