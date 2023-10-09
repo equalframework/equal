7d14f0b21830b8792ec5809d6b94a6006660d955
Cédric Françoys <cedricfrancoys@gmail.com>
2023-10-09 17:16:50 +0200

    added explicit class for announce call

--- packages/core/data/installed-apps.php
+++ packages/core/data/installed-apps.php
@@ -4,7 +4,7 @@
     Some Rights Reserved, Cedric Francoys, 2010-2021
     Licensed under GNU LGPL 3 license <http://www.gnu.org/licenses/>
 */
-list($params, $providers) = announce([
+list($params, $providers) = eQual::announce([
     'description'   => "Returns a map with the descriptors of the installed apps (depending on initialized packages).",
     'access'        => [
         'visibility'    => 'protected'
