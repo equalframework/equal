bbded3840cdaaddf65f375d7364275c7493433e7
Cédric Françoys <cedricfrancoys@gmail.com>
2023-10-28 09:13:11 +0200

    comments

--- packages/core/actions/config/save-model.php
+++ packages/core/actions/config/save-model.php
@@ -22,8 +22,13 @@ list($params, $providers) = announce([
             'required'      => true
         ],
         'part' => [
-            'description' => '',
-            'type' => 'string',
+            'description'   => '',
+            'type'          => 'string',
+            'selection'     => [
+                'class',        // #todo - replace with 'columns'
+                'workflow',
+                'roles'
+            ],
             'required'      => true
         ],
         'payload' =>  [
@@ -32,10 +37,14 @@ list($params, $providers) = announce([
             'required'      => true
         ]
     ],
-    'providers'     => ['context', 'orm', 'adapt']
+    'providers'     => ['context', 'orm']
 ]);
 
-list($context, $orm, $adapter) = [$providers['context'], $providers['orm'], $providers['adapt']];
+/**
+ * @var \equal\php\Context          $context
+ * @var \equal\orm\ObjectManager    $orm
+ */
+list($context, $orm) = [$providers['context'], $providers['orm']];
 
 // Create all the object to use for using PhpParser
 $parser = (new ParserFactory)->create(ParserFactory::PREFER_PHP7);
@@ -49,85 +58,95 @@ $parts = explode('\\', $params['entity']);
 // Get the package name from the first part of the string
 $package = array_shift($parts);// Get the package name from the first part of the string
 // Get the file name from the last part of the string
-$file = array_pop($parts);
+$filename = array_pop($parts);
 // Get the class path from the remaining part
 $class_path = implode('/', $parts);
 
 $getColumns = null;
 $code_php = null;
 
+// Get the full path of the file
+$file = QN_BASEDIR."/packages/{$package}/classes/{$class_path}/{$filename}.class.php";
+
 // If you want to use save-model for updating a class/getColumns
 if($params['part'] == 'class') {
     // Decode the JSON string to a PHP object
-    $code_php = deleteModelProperty($params['payload']['fields']);
+    $code_php = sanitizeColumns($params['payload']['fields']);
 
     // Get the string representation of the code_php variable, with backslashes escaped
     $code_string = str_replace("\\\\", "\\", var_export($code_php, true) );
 
-    // Get the full path to the file
-    $file = QN_BASEDIR."/packages/{$package}/classes/{$class_path}/{$file}.class.php";
 
-    // #todo - handle class creation in controller create-model
-
-    // Create a temporary file with the following contents and then parse it to have a ast
-    $temp_file = "<?php \nclass temp { public static function getColumns() { return ".$code_string.";}}";
-    $ast_temp_file= $parser->parse($temp_file);
+    // Create a temporary file with the following contents and then parse it in order to generate a minimal AST
+    $ast_temp = $parser->parse("<?php \nclass temp { public static function getColumns() { return ".$code_string.";}}");
 
     // Find the getColumns node in the AST of the temporary file
-    $nodeGetColumns = $nodeFinder->findFirst($ast_temp_file, function(Node $node) {
-        return $node->name->name === "getColumns";
-    });
+    $nodeGetColumns = $nodeFinder->findFirst($ast_temp, function(Node $node) {return $node->name->name === "getColumns";});
 
-    // Add a visitor to the traverse
-    $traverser->addVisitor(new class($nodeGetColumns, "getColumns") extends NodeVisitorAbstract {
+    // Add a visitor to the traverse in order to consider the getColumns method and update its content with new schema
+    $traverser->addVisitor(
+            new class($nodeGetColumns, "getColumns") extends NodeVisitorAbstract {
 
-        private $node;
-        private $target;
+                private $node;
+                private $target;
 
-        public function __construct($node, $target) {
-            $this->node = $node;
-            $this->target = $target;
-        }
+                public function __construct($node, $target) {
+                    $this->node = $node;
+                    $this->target = $target;
+                }
 
-        public function leaveNode(Node $node) {
-            if ($node->name->name === $this->target) {
-                return $this->node;
+                public function leaveNode(Node $node) {
+                    if ($node->name->name === $this->target) {
+                        return $this->node;
+                    }
+                }
             }
-        }
-    });
+        );
 
 
-    // Add a visitor to the traverser that will set the doc comment of the class node to the result of the getPropertiesAsComments function
-    $traverser->addVisitor(new class($code_php) extends NodeVisitorAbstract {
-        private $code_php;
-        public function __construct($code_php) {
-            $this->code_php = $code_php;
-        }
+    // Add a visitor to the traverser for setting the doc comments on the class node
+    $traverser->addVisitor(
+            new class($code_php) extends NodeVisitorAbstract {
 
-        public function leaveNode(Node $node) {
-            if ($node instanceof Node\Stmt\Class_) {
-                $node->setDocComment(new Comment\Doc(getPropertiesAsComments($this->code_php)));
-                return NodeTraverser::STOP_TRAVERSAL;
+                private $code_php;
+
+                public function __construct($code_php) {
+                    $this->code_php = $code_php;
+                }
+
+                public function leaveNode(Node $node) {
+                    if ($node instanceof Node\Stmt\Class_) {
+                        $node->setDocComment(new Comment\Doc(getPropertiesAsComments($this->code_php)));
+                        return NodeTraverser::STOP_TRAVERSAL;
+                    }
+                }
             }
-        }
-    });
+        );
 }
 
-// Get the code from the file and parse it in a AST
+// Get the code from the original file ...
 $code = file_get_contents($file);
+// ... and parse it to create an AST
 $stmts = $parser->parse($code);
 
-// Traverse the AST of the code with the traverser
+// Update the AST by using visitors attached to the traverser
 $modifiedStmts = $traverser->traverse($stmts);
 
-// Pretty print the modified AST and write a pretty code into the file
+// Pretty print the modified AST ...
 $result = $prettyPrinter->prettyPrintFile($modifiedStmts);
+// ... and write back the code to the file
 file_put_contents($file, $result);
 
-$command = 'php ./vendor/bin/ecs check "' . str_replace('\\', '/', $file) . '" --fix';
+try {
+    // apply coding standards (ecs.php is expected in QN_BASEDIR)
+    $command = 'php ./vendor/bin/ecs check "' . str_replace('\\', '/', $file) . '" --fix';
 
-if(exec($command) === false) {
-    throw new Exception('command_failed', QN_ERROR_UNKNOWN);
+    if(exec($command) === false) {
+        throw new Exception('command_failed', QN_ERROR_UNKNOWN);
+    }
+}
+catch(Exception $e) {
+    // #todo - trigger a warning
 }
 
 $result = file_get_contents($file);
@@ -143,13 +162,13 @@ $context->httpResponse()
  *
  * @return array    A new array without the properties already inherited from Model
  */
-function deleteModelProperty($properties) {
+function sanitizeColumns($properties) {
     $result = [];
-    $specialColumns =  Model::getSpecialColumns();
+    $special_columns = Model::getSpecialColumns();
     foreach($properties as $property => $descriptor) {
-        if(array_key_exists($property, $specialColumns)){
-            if($descriptor != $specialColumns[$property]){
-                if($specialColumns[$property]["type"] != 'datetime') {
+        if(array_key_exists($property, $special_columns)){
+            if($descriptor != $special_columns[$property]){
+                if($special_columns[$property]["type"] != 'datetime') {
                     $result[$property] = $descriptor;
                 }
             }
@@ -165,8 +184,8 @@ function deleteModelProperty($properties) {
 /**
  * This function gets the comments for the properties of the model
  *
- * @param Array the properties which need to be in the doc
- * @return string of the documentation
+ * @param   array   The properties which need to be in the doc.
+ * @return  string  PHP Doc string.
  */
 function getPropertiesAsComments($properties) {
     $comment = "/**\n";
