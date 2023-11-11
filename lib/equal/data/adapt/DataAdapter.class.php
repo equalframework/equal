619b50805ee56779e83adb1451cc5c057793a631
Cédric Françoys <cedricfrancoys@gmail.com>
2023-11-11 20:42:56 +0100

    refactored as interface

--- lib/equal/data/adapt/DataAdapter.class.php
+++ lib/equal/data/adapt/DataAdapter.class.php
@@ -1,95 +1,39 @@
 <?php
 /*
-    This file is part of the eQual framework <http://www.github.com/cedricfrancoys/equal>
-    Some Rights Reserved, Cedric Francoys, 2010-2021
+    This file is part of the eQual framework <https://github.com/equalframework/equal>
+    Some Rights Reserved, Cedric Francoys, 2010-2024
     Licensed under GNU LGPL 3 license <http://www.gnu.org/licenses/>
 */
 namespace equal\data\adapt;
 
 
-class DataAdapter implements DataAdapterInterface {
-
-    /** @var string */
-    protected $type = '';
-
-    public function __construct() {
-    }
-
-    public function getType() {
-        return $this->type;
-    }
-
-    public static function strToInteger($value) {
-        if(is_string($value)) {
-            if($value == 'null') {
-                $value = null;
-            }
-            elseif(empty($value)) {
-                $value = 0;
-            }
-            elseif(in_array($value, ['TRUE', 'true'])) {
-                $value = 1;
-            }
-            elseif(in_array($value, ['FALSE', 'false'])) {
-                $value = 0;
-            }
-        }
-        // arg represents a numeric value (numeric type or string)
-        if(is_numeric($value)) {
-            $value = intval($value);
-        }
-        elseif(is_scalar($value)) {
-            $suffixes = [
-                'B'  => 1,
-                'KB' => 1024,
-                'MB' => 1048576,
-                'GB' => 1073741824,
-                's'  => 1,
-                'm'  => 60,
-                'h'  => 3600,
-                'd'  => 3600*24,
-                'w'  => 3600*24*7,
-                'M'  => 3600*24*30,
-                'Y'  => 3600*24*365
-            ];
-            $val = (string) $value;
-            $intval = intval($val);
-            foreach($suffixes as $suffix => $factor) {
-                if(strval($intval).$suffix == $val) {
-                    $value = $intval * $factor;
-                    break;
-                }
-            }
-        }
-        return $value;
-    }
+interface DataAdapter {
 
     /**
-     * Default method for data adaptation
-     * This is used as fallback when usage did not trigger any other adaptation, and simply return the original value unchanged.
+     * Provides the type of the adapter.
+     * @return string
      */
-    protected function adaptDefault($value) {
-        return $value;
-    }
+    public function getType();
 
     /**
      * Handles the conversion to the PHP type equivalent.
-     * This method is meant to be overloaded by children classes and called as fallback.
-     * Conversion: x -> PHP
+     * Adapts the input value from external type to PHP type (x -> PHP).
      *
+     * @param mixed         $value      Value to be adapted.
+     * @param string|Usage  $usage      The usage descriptor the adaptation is requested for.
+     * @param string        $locale     The locale to be used for adaptation.
+     * @return mixed
      */
-	public function adaptIn($value, $usage, $lang='en') {
-        return $this->adaptDefault($value);
-    }
+	public function adaptIn($value, $type, $locale='en');
 
     /**
      * Handles the conversion to the type targeted by the DataAdapter.
-     * This method is meant to be overloaded by children classes and called as fallback.
-     * Conversion: PHP -> x
+     * Adapts the input value from PHP type to external type (PHP -> x).
      *
+     * @param mixed         $value      Value to be adapted.
+     * @param string|Usage  $usage      The usage descriptor the adaptation is requested for.
+     * @param string        $locale     The locale to be used for adaptation.
+     * @return mixed
      */
-    public function adaptOut($value, $usage, $lang='en') {
-        return $this->adaptDefault($value);
-    }
-
+    public function adaptOut($value, $type, $locale='en');
 }
