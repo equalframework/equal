343a6a164d50cc18586ff8ebc84141bc4cbba11a
Cédric Françoys <cedricfrancoys@gmail.com>
2023-12-04 14:36:18 +0100

    improved (removed unnecessary)

--- packages/core/actions/alert/dismiss.php
+++ packages/core/actions/alert/dismiss.php
@@ -4,12 +4,9 @@
     Some Rights Reserved, Yesbabylon SRL, 2020-2021
     Licensed under GNU AGPL 3 license <http://www.gnu.org/licenses/>
 */
-use sale\booking\Consumption;
-use lodging\sale\booking\BookingLine;
-use lodging\sale\booking\Booking;
 
-list($params, $providers) = announce([
-    'description'   => "Tries to dismiss a message. Should be invoked as a user request for removing the message. If the situation is still occuring an identical alert will be re-created.",
+list($params, $providers) = eQual::announce([
+    'description'   => "Tries to dismiss a message. Should be invoked as a user request for removing the message. If the situation is still occurring an identical alert will be re-created.",
     'params'        => [
         'id' =>  [
             'description'   => 'Identifier of the alert to dismiss.',
@@ -25,29 +22,22 @@ list($params, $providers) = announce([
         'charset'       => 'utf-8',
         'accept-origin' => '*'
     ],
-    'providers'     => ['context', 'orm', 'auth', 'dispatch']
+    'providers'     => ['context', 'auth', 'dispatch']
 ]);
 
 /**
  * @var \equal\php\Context $context
- * @var \equal\orm\ObjectManager $orm
  * @var \equal\auth\AuthenticationManager $auth
  * @var \equal\dispatch\Dispatcher $dispatch
  */
-list($context, $orm, $auth, $dispatch) = [ $providers['context'], $providers['orm'], $providers['auth'], $providers['dispatch']];
+list($context, $auth, $dispatch) = [ $providers['context'], $providers['auth'], $providers['dispatch']];
 
-// #todo - restrict access based on link between messagemodel and user groups
-
-// #todo - deprecate (visibility = protected)
 $user_id = $auth->userId();
 
-if($user_id <= 0) {
-    throw new Exception("not_allowed", QN_ERROR_NOT_ALLOWED);
-}
+// #todo - restrict access based on link between MessageModel and user groups
 
 // If the alert is not found, the call is ignored. If a controller is mentioned in the alert it is called.
 $dispatch->dismiss($params['id']);
 
 $context->httpResponse()
-        ->status(200)
         ->send();
