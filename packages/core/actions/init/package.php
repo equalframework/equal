e1b7ca92dad46c09db9c8604730244ae0b8af444
Cédric Françoys <cedricfrancoys@gmail.com>
2023-12-10 15:38:57 +0100

    fixed creation with predefined id

--- packages/core/actions/init/package.php
+++ packages/core/actions/init/package.php
@@ -129,7 +129,7 @@ if($params['import'] && file_exists($data_folder) && is_dir($data_folder)) {
     foreach (glob($data_folder."/*.json") as $json_file) {
         $data = file_get_contents($json_file);
         $classes = json_decode($data, true);
-        foreach($classes as $class){
+        foreach($classes as $class) {
             $entity = $class['name'];
             $lang = $class['lang'];
             $model = $orm->getModel($entity);
@@ -142,15 +142,16 @@ if($params['import'] && file_exists($data_folder) && is_dir($data_folder)) {
                     $f = new Field($schema[$field]);
                     $odata[$field] = $adapter->adaptIn($value, $f->getUsage());
                 }
+
                 if(isset($odata['id'])) {
                     $res = $orm->search($entity, ['id', '=', $odata['id']]);
                     if($res > 0 && count($res)) {
                         // object already exist, but either values or language might differ
-                        $id = $odata['id'];
                     }
                     else {
-                        $id = $orm->create($entity, [], $lang);
+                        $orm->create($entity, ['id' => $odata['id']], $lang, false);
                     }
+                    $id = $odata['id'];
                     unset($odata['id']);
                 }
                 else {
