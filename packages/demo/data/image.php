b199080d4882753432ddfab53ad18080b0084b85
Cédric Françoys <cedricfrancoys@gmail.com>
2023-12-11 15:44:21 +0100

    syntax

--- packages/demo/data/image.php
+++ packages/demo/data/image.php
@@ -5,7 +5,7 @@ use equal\http\HttpRequest;
  * HTTP native support
  *
  */
-list($params, $providers) = announce([
+list($params, $providers) = eQual::announce([
     'description'   => 'Get picture data from imgur.com using imgur API.',
     'params'        => [
         'id' => [
@@ -26,7 +26,7 @@ $request = new httpRequest("GET https://api.imgur.com/3/image/{$params['id']}");
 $response = $request
             ->header('Authorization', "Client-ID 34030ab1f5ef12d")
             ->send();
-           
+
 $providers['context']
     ->httpResponse()
     ->body(['result' => $response->body()])
