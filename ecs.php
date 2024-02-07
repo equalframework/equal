62427854966e6754c3f631fcc1ef3709fd8f166c
Cédric Françoys <cedricfrancoys@gmail.com>
2024-02-07 14:15:05 +0100

    adapted for output improvement

--- ecs.php
+++ ecs.php
@@ -9,20 +9,21 @@ use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 return static function (ECSConfig $ecsConfig): void {
     $ecsConfig->paths([__DIR__ . '/src', __DIR__ . '/tests']);
 
-    $ecsConfig->ruleWithConfiguration(ArraySyntaxFixer::class, [
-        'syntax' => 'short',
-    ]);
-
     $ecsConfig->sets([
         // run and fix, one by one
         SetList::SPACES,
         SetList::ARRAY,
         SetList::DOCBLOCK,
-        SetList::SYMPLIFY,
+        // #memo - messes with COMMENTS blocks
+        // SetList::SYMPLIFY,
         // #memo - adds declare(strict_types=1)
         // SetList::COMMON,
         SetList::CLEAN_CODE,
-        // #memo - join comment lines and then break them without starting with * (star)
-        // SetList::PSR_12,
+        SetList::PSR_12,
     ]);
+
+    $ecsConfig->ruleWithConfiguration(ArraySyntaxFixer::class, [
+        'syntax' => 'short',
+    ]);
+
 };
