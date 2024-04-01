c93c6f7aa585325ecce4f35c5f7f55b589051a8c
Cédric Françoys <cedricfrancoys@gmail.com>
2024-04-01 19:28:58 +0200

    fixed conditions tests

--- lib/equal/orm/usages/UsageNumber.class.php
+++ lib/equal/orm/usages/UsageNumber.class.php
@@ -69,10 +69,7 @@ class UsageNumber extends Usage {
                     'not_integer' => [
                         'message'   => 'Value is not a boolean.',
                         'function'  =>  function($value) {
-                            if(!preg_match('/^[0-1]$/', (string) $value)) {
-                                return false;
-                            }
-                            return true;
+                            return preg_match('/^[0-1]$/', (string) $value);
                         }
                     ]
                 ];
@@ -84,10 +81,7 @@ class UsageNumber extends Usage {
                             $len = intval($this->getLength());
                             // if length is empty, default to 18 (max)
                             $len = ($len)?$len:18;
-                            if(!preg_match('/^[+-]?[0-9]{0,'.$len.'}$/', (string) $value)) {
-                                return false;
-                            }
-                            return true;
+                            return preg_match('/^[+-]?[0-9]{0,'.$len.'}$/', (string) $value);
                         }
                     ]
                 ];
@@ -106,10 +100,7 @@ class UsageNumber extends Usage {
                             // expected len format is `precision.scale`
                             $scale = $this->getScale();
                             $integers = $this->getPrecision();
-                            if(preg_match('/^[+-]?[0-9]{0,'.$integers.'}(\.[0-9]{1,'.$scale.'})?$/', (string) $value)) {
-                                return false;
-                            }
-                            return true;
+                            return preg_match('/^[+-]?[0-9]{0,'.$integers.'}(\.[0-9]{1,'.$scale.'})?$/', (string) $value);
                         }
                     ]
                 ];
@@ -121,10 +112,7 @@ class UsageNumber extends Usage {
                             $len = intval($this->getLength());
                             // if length is empty, default to 255 (max)
                             $len = ($len)?$len:255;
-                            if(preg_match('/^[0-9A-F]{0,'.$len.'}$/', (string) $value)) {
-                                return false;
-                            }
-                            return true;
+                            return preg_match('/^[0-9A-F]{0,'.$len.'}$/', (string) $value);
                         }
                     ]
                 ];
