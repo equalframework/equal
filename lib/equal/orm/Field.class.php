e9fbb23a7efd8d0c284ba998dc89267d1211e65d
Cédric Françoys <cedricfrancoys@gmail.com>
2024-04-02 14:36:45 +0200

    fixed support for computed fields

--- lib/equal/orm/Field.class.php
+++ lib/equal/orm/Field.class.php
@@ -87,32 +87,29 @@ class Field {
      * @return array
      */
     public function getConstraints(): array {
-        static $map = [
-            'bool'      => 'boolean',
-            'int'       => 'integer',
-            'float'     => 'double',
-            'text'      => 'string',
-            'date'      => 'integer',
-            'datetime'  => 'integer',
-            'file'      => 'string',
-            'binary'    => 'string',
-            'many2one'  => 'integer',
-            'one2many'  => 'array',
-            'many2many' => 'array'
-        ];
-
-        // fix types to match values returned by PHP `gettype()`
-        $result_type = $this->type;
-        if(isset($map[$this->type])) {
-            $result_type = $map[$this->type];
-        }
-
         // generate constraint based on type
+        $result_type = $this->descriptor['result_type'];
+
         $constraints = [
             'invalid_type' => [
-                'message'   => "Value is not of type {$this->type}.",
+                'message'   => "Value is not of type {$result_type}.",
                 'function'  =>  function($value) use($result_type) {
-                    return (gettype($value) == $result_type);
+                    static $map = [
+                        'bool'      => 'boolean',
+                        'int'       => 'integer',
+                        'float'     => 'double',
+                        'text'      => 'string',
+                        'date'      => 'integer',
+                        'datetime'  => 'integer',
+                        'file'      => 'string',
+                        'binary'    => 'string',
+                        'many2one'  => 'integer',
+                        'one2many'  => 'array',
+                        'many2many' => 'array'
+                    ];
+                    // fix types to match values returned by PHP `gettype()`
+                    $mapped_type = $map[$result_type] ?? $result_type;
+                    return (gettype($value) == $mapped_type);
                 }
             ]
         ];
