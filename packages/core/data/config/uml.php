bf6488bda50de799a68a5a0443f45ae5c7c899f1
Cédric Françoys <cedricfrancoys@gmail.com>
2024-04-11 15:20:35 +0200

    updated to handle .erd.json extension

--- packages/core/data/config/uml.php
+++ packages/core/data/config/uml.php
@@ -1,4 +1,9 @@
 <?php
+/*
+    This file is part of the eQual framework <http://www.github.com/cedricfrancoys/equal>
+    Some Rights Reserved, Cedric Francoys, 2010-2024
+    Licensed under GNU LGPL 3 license <http://www.gnu.org/licenses/>
+*/
 
 list($params, $providers) = eQual::announce([
     'description'   => "Attempts to create a new package using a given name.",
--- packages/core/data/config/umls.php
+++ packages/core/data/config/umls.php
@@ -1,5 +1,9 @@
 <?php
-
+/*
+    This file is part of the eQual framework <http://www.github.com/cedricfrancoys/equal>
+    Some Rights Reserved, Cedric Francoys, 2010-2024
+    Licensed under GNU LGPL 3 license <http://www.gnu.org/licenses/>
+*/
 
 list($params, $providers) = eQual::announce([
     'description'   => 'Returns the list of menus defined in a given package, or applicable to a given entity.',
