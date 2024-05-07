727b3e8a38fe1f955f7c98735959d9d66b8867d9
Cédric Françoys <cedricfrancoys@gmail.com>
2024-05-07 15:08:29 +0200

    added explicit basedir + splitted install or update

--- packages/core/actions/init/composer.php
+++ packages/core/actions/init/composer.php
@@ -1,14 +1,14 @@
 <?php
 /*
-    This file is part of the eQual framework <http://www.github.com/cedricfrancoys/equal>
-    Some Rights Reserved, Cedric Francoys, 2010-2021
+    This file is part of the eQual framework <http://www.github.com/equalframework/equal>
+    Some Rights Reserved, Cedric Francoys, 2010-2024
     Licensed under GNU GPL 3 license <http://www.gnu.org/licenses/>
 */
 use equal\http\HttpRequest;
 use equal\http\HttpResponse;
 
 
-list($params, $providers) = announce([
+list($params, $providers) = eQual::announce([
     'description'   => "Downloads composer and runs it for installing dependencies from composer.json.",
     'help'          => "This controller rely on the PHP binary. In order to make them work, sure the PHP binary is present in the PATH.",
     'params'        => [],
@@ -21,43 +21,53 @@ list($params, $providers) = announce([
 list($context) = [$providers['context']];
 
 // stop if composer.json is missing
-if(!file_exists('composer.json')) {
-    throw new Exception('missing_composer_json', QN_ERROR_MISSING_PARAM);
+if(!file_exists(EQ_BASEDIR.'/composer.json')) {
+    throw new Exception('missing_composer_json', EQ_ERROR_MISSING_PARAM);
 }
 
-// retrieve the checksum on github
-$request = new HttpRequest('GET https://composer.github.io/installer.sig');
-/** @var HttpResponse */
-$response = $request->send();
-$expected_checksum = $response->body();
+if(!file_exists(EQ_BASEDIR.'/composer.phar')) {
+    // retrieve the checksum on github
+    $request = new HttpRequest('GET https://composer.github.io/installer.sig');
+    /** @var HttpResponse */
+    $response = $request->send();
+    $expected_checksum = $response->body();
 
-// download composer-setup script
-copy('https://getcomposer.org/installer', 'composer-setup.php');
+    // download composer-setup script
+    copy('https://getcomposer.org/installer', EQ_BASEDIR.'/composer-setup.php');
 
-// if something went wrong during download, stop
-if(!file_exists('composer-setup.php')) {
-    throw new Exception('missing_file', QN_ERROR_UNKNOWN_OBJECT);
+    // if something went wrong during download, stop
+    if(!file_exists(EQ_BASEDIR.'/composer-setup.php')) {
+        throw new Exception('missing_file', EQ_ERROR_UNKNOWN_OBJECT);
+    }
+
+    // make sure checksum if consistent
+    if(hash_file('sha384', EQ_BASEDIR.'/composer-setup.php') !== $expected_checksum) {
+        throw new Exception('invalid_checksum', EQ_ERROR_UNKNOWN_OBJECT);
+    }
+
+    // run setup and remove script afterward
+    if(exec('php composer-setup.php --quiet') === false) {
+        throw new Exception('command_failed', EQ_ERROR_UNKNOWN);
+    }
+    unlink(EQ_BASEDIR.'/composer-setup.php');
+
+    // check the presence of the executable
+    if(!file_exists(EQ_BASEDIR.'/composer.phar')) {
+        throw new Exception('install_failed', EQ_ERROR_UNKNOWN);
+    }
 }
 
-// make sure checksum if consistent
-if(hash_file('sha384', 'composer-setup.php') !== $expected_checksum) {
-    throw new Exception('invalid_checksum', QN_ERROR_UNKNOWN_OBJECT);
+if(!file_exists(EQ_BASEDIR.'/composer.lock')) {
+    // run composer to install dependencies (quiet mode, no interactions)
+    if(exec('php composer.phar install -q -n') === false) {
+        throw new Exception('composer_failed', EQ_ERROR_UNKNOWN);
+    }
 }
-
-// run setup and remove script afterward
-if(exec('php composer-setup.php --quiet') === false) {
-    throw new Exception('command_failed', QN_ERROR_UNKNOWN);
-}
-unlink('composer-setup.php');
-
-// check the presence of the executable
-if(!file_exists('composer.phar')) {
-    throw new Exception('install_failed', QN_ERROR_UNKNOWN);
-}
-
-// run composer to install dependencies (quiet mode, no interactions)
-if(exec('php composer.phar install -q -n') === false) {
-    throw new Exception('composer_failed', QN_ERROR_UNKNOWN);
+else {
+    // run composer update
+    if(exec('php composer.phar update -q -n') === false) {
+        throw new Exception('composer_failed', EQ_ERROR_UNKNOWN);
+    }
 }
 
 $context->httpResponse()
