952c332aad810da324dbac00159c0165b98a2448
Cédric Françoys <cedricfrancoys@gmail.com>
2024-05-12 12:55:38 +0200

    debug

--- lib/equal/orm/Model.class.php
+++ lib/equal/orm/Model.class.php
@@ -122,7 +122,7 @@ class Model implements \ArrayAccess, \Iterator {
                     ob_start();
                     var_dump($defaults[$field]);
                     $log = ob_get_clean();
-                    trigger_error("ORM::Model trying to default with {$log}", QN_REPORT_DEBUG);
+                    trigger_error("ORM::Model ".$this->getType()." for field $field - trying to default with {$log}", QN_REPORT_DEBUG);
                     if(method_exists($this->getType(), $defaults[$field])) {
                         $this->values[$field] = $orm->callonce($this->getType(), $defaults[$field]);
                     }
