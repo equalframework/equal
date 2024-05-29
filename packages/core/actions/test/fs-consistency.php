0d6be9180334fd294b0b5c98a309bdfb43efb1a5
Cédric Françoys <cedricfrancoys@gmail.com>
2024-05-29 14:05:36 +0200

    completed FS checks for UGO

--- packages/core/actions/test/fs-consistency.php
+++ packages/core/actions/test/fs-consistency.php
@@ -1,7 +1,7 @@
 <?php
 /*
-    This file is part of the eQual framework <http://www.github.com/cedricfrancoys/equal>
-    Some Rights Reserved, Cedric Francoys, 2010-2021
+    This file is part of the eQual framework <http://www.github.com/equalframework/equal>
+    Some Rights Reserved, Cedric Francoys, 2010-2024
     Licensed under GNU GPL 3 license <http://www.gnu.org/licenses/>
 */
 $params = eQual::announce([
@@ -18,31 +18,31 @@ $paths = [
     ],
     [
         'rights'    =>  QN_R_READ | QN_R_WRITE,
-        'path'      =>  QN_BASEDIR.'/cache'
+        'path'      =>  EQ_BASEDIR.'/cache'
     ],
     [
         'rights'    =>  QN_R_READ | QN_R_WRITE,
-        'path'      =>  QN_BASEDIR.'/bin'
+        'path'      =>  EQ_BASEDIR.'/bin'
     ],
     [
         'rights'    =>  QN_R_READ | QN_R_WRITE,
-        'path'      =>  QN_BASEDIR.'/spool'
+        'path'      =>  EQ_BASEDIR.'/spool'
     ],
     [
         'rights'    =>  QN_R_READ,
-        'path'      =>  QN_BASEDIR.'/lib'
+        'path'      =>  EQ_BASEDIR.'/lib'
     ],
     [
         'rights'    =>  QN_R_READ,
-        'path'      =>  QN_BASEDIR.'/config'
+        'path'      =>  EQ_BASEDIR.'/config'
     ],
     [
         'rights'    =>  QN_R_READ,
-        'path'      =>  QN_BASEDIR.'/config/schema.json'
+        'path'      =>  EQ_BASEDIR.'/config/schema.json'
     ],
     [
         'rights'    =>  QN_R_READ,
-        'path'      =>  QN_BASEDIR.'/config/routing'
+        'path'      =>  EQ_BASEDIR.'/config/routing'
     ]
 ];
 
@@ -50,7 +50,7 @@ $paths = [
 if(constant('FILE_STORAGE_MODE') == 'FS') {
     $paths[] = [
         'rights'    =>  QN_R_READ | QN_R_WRITE,
-        'path'      =>  QN_BASEDIR.'/bin'
+        'path'      =>  EQ_BASEDIR.'/bin'
     ];
 }
 
@@ -72,22 +72,31 @@ function check_permissions($path, $mask, $uid=0) {
         // get the group owner of the file
         $fgid = filegroup($path);
 
+        // check user permissions
         if($fuid == $uid) {
-            // check user perms
-            if($mask & QN_R_READ) {
-                if(!($perms & 0x0100)) return -QN_R_READ;
+            if( ($mask & QN_R_READ) && !($perms & 0x0100)) {
+                return -QN_R_READ;
             }
-            if($mask & QN_R_WRITE) {
-                if(!($perms & 0x0080)) return -QN_R_WRITE;
+            if( ($mask & QN_R_WRITE) && !($perms & 0x0080)) {
+                return -QN_R_WRITE;
             }
         }
+        // check group permissions
         else if($fgid == $uid) {
-            // check group perms
-            if($mask & QN_R_READ) {
-                if(!($perms & 0x0020)) return -QN_R_READ;
+            if( ($mask & QN_R_READ) && !($perms & 0x0020)) {
+                return -QN_R_READ;
             }
-            if($mask & QN_R_WRITE) {
-                if(!($perms & 0x0010)) return -QN_R_WRITE;
+            if( ($mask & QN_R_WRITE) && !($perms & 0x0010)) {
+                return -QN_R_WRITE;
+            }
+        }
+        // check others permission
+        else {
+            if( ($mask & QN_R_READ) && !($perms & 0x0004)) {
+                return -QN_R_READ;
+            }
+            if( ($mask & QN_R_WRITE) && !($perms & 0x0002)) {
+                return -QN_R_WRITE;
             }
         }
     }
@@ -99,7 +108,7 @@ $uid = 0;
 
 $username = constant('HTTP_PROCESS_USERNAME');
 
-// get UID of a use by its name
+// get UID of a user by its name
 exec("id -u \"$username\" 2>&1", $output);
 
 if(count($output)) {
@@ -129,6 +138,5 @@ foreach($paths as $item) {
         throw new Exception("PHP or HTTP process has no {$missing} access on {$item['path']}", QN_ERROR_INVALID_CONFIG);
     }
 }
-
 
 // no error, exit code will be 0
