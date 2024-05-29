a3b5000caba53c1df87a11d2b09f96c82a357bd6
Cédric Françoys <cedricfrancoys@gmail.com>
2024-05-29 14:31:43 +0200

    minor refactor

--- packages/core/actions/test/fs-consistency.php
+++ packages/core/actions/test/fs-consistency.php
@@ -5,7 +5,7 @@
     Licensed under GNU GPL 3 license <http://www.gnu.org/licenses/>
 */
 $params = eQual::announce([
-    'description'   => 'Checks current installation directories integrity',
+    'description'   => 'Checks current installation directories integrity.',
     'params'        => [],
     'constants'     => ['FILE_STORAGE_MODE', 'HTTP_PROCESS_USERNAME']
 ]);
@@ -13,35 +13,31 @@ $params = eQual::announce([
 // array holding files and directories to be tested
 $paths = [
     [
-        'rights'    =>  QN_R_READ | QN_R_WRITE,
+        'rights'    =>  EQ_R_READ | EQ_R_WRITE,
         'path'      =>  QN_LOG_STORAGE_DIR
     ],
     [
-        'rights'    =>  QN_R_READ | QN_R_WRITE,
+        'rights'    =>  EQ_R_READ | EQ_R_WRITE,
         'path'      =>  EQ_BASEDIR.'/cache'
     ],
     [
-        'rights'    =>  QN_R_READ | QN_R_WRITE,
+        'rights'    =>  EQ_R_READ | EQ_R_WRITE,
         'path'      =>  EQ_BASEDIR.'/bin'
     ],
     [
-        'rights'    =>  QN_R_READ | QN_R_WRITE,
+        'rights'    =>  EQ_R_READ | EQ_R_WRITE,
         'path'      =>  EQ_BASEDIR.'/spool'
     ],
     [
-        'rights'    =>  QN_R_READ,
+        'rights'    =>  EQ_R_READ,
         'path'      =>  EQ_BASEDIR.'/lib'
     ],
     [
-        'rights'    =>  QN_R_READ,
+        'rights'    =>  EQ_R_READ,
         'path'      =>  EQ_BASEDIR.'/config'
     ],
     [
-        'rights'    =>  QN_R_READ,
-        'path'      =>  EQ_BASEDIR.'/config/schema.json'
-    ],
-    [
-        'rights'    =>  QN_R_READ,
+        'rights'    =>  EQ_R_READ,
         'path'      =>  EQ_BASEDIR.'/config/routing'
     ]
 ];
@@ -49,7 +45,7 @@ $paths = [
 
 if(constant('FILE_STORAGE_MODE') == 'FS') {
     $paths[] = [
-        'rights'    =>  QN_R_READ | QN_R_WRITE,
+        'rights'    =>  EQ_R_READ | EQ_R_WRITE,
         'path'      =>  EQ_BASEDIR.'/bin'
     ];
 }
@@ -57,11 +53,15 @@ if(constant('FILE_STORAGE_MODE') == 'FS') {
 
 function check_permissions($path, $mask, $uid=0) {
     if(!$uid) {
-        if($mask & QN_R_READ) {
-            if(!is_readable($path)) return -QN_R_READ;
+        if($mask & EQ_R_READ) {
+            if(!is_readable($path)) {
+                return -EQ_R_READ;
+            }
         }
-        if($mask & QN_R_WRITE) {
-            if(!is_writable($path)) return -QN_R_WRITE;
+        if($mask & EQ_R_WRITE) {
+            if(!is_writable($path)) {
+                return -EQ_R_WRITE;
+            }
         }
     }
     else {
@@ -74,29 +74,29 @@ function check_permissions($path, $mask, $uid=0) {
 
         // check user permissions
         if($fuid == $uid) {
-            if( ($mask & QN_R_READ) && !($perms & 0x0100)) {
-                return -QN_R_READ;
+            if( ($mask & EQ_R_READ) && !($perms & 0x0100)) {
+                return -EQ_R_READ;
             }
-            if( ($mask & QN_R_WRITE) && !($perms & 0x0080)) {
-                return -QN_R_WRITE;
+            if( ($mask & EQ_R_WRITE) && !($perms & 0x0080)) {
+                return -EQ_R_WRITE;
             }
         }
         // check group permissions
         else if($fgid == $uid) {
-            if( ($mask & QN_R_READ) && !($perms & 0x0020)) {
-                return -QN_R_READ;
+            if( ($mask & EQ_R_READ) && !($perms & 0x0020)) {
+                return -EQ_R_READ;
             }
-            if( ($mask & QN_R_WRITE) && !($perms & 0x0010)) {
-                return -QN_R_WRITE;
+            if( ($mask & EQ_R_WRITE) && !($perms & 0x0010)) {
+                return -EQ_R_WRITE;
             }
         }
         // check others permission
         else {
-            if( ($mask & QN_R_READ) && !($perms & 0x0004)) {
-                return -QN_R_READ;
+            if( ($mask & EQ_R_READ) && !($perms & 0x0004)) {
+                return -EQ_R_READ;
             }
-            if( ($mask & QN_R_WRITE) && !($perms & 0x0002)) {
-                return -QN_R_WRITE;
+            if( ($mask & EQ_R_WRITE) && !($perms & 0x0002)) {
+                return -EQ_R_WRITE;
             }
         }
     }
@@ -116,26 +116,26 @@ if(count($output)) {
 }
 
 if(!$uid) {
-    throw new Exception(serialize(['unknown_user' => $username]), QN_ERROR_INVALID_CONFIG);
+    throw new Exception(serialize(['unknown_user' => $username]), EQ_ERROR_INVALID_CONFIG);
 }
 
 // check mod
 foreach($paths as $item) {
     if(!file_exists($item['path'])) {
-        throw new Exception(serialize(['missing_mandatory_node' => $item['path']]), QN_ERROR_INVALID_CONFIG);
+        throw new Exception(serialize(['missing_mandatory_node' => $item['path']]), EQ_ERROR_INVALID_CONFIG);
     }
     if( ($res = check_permissions($item['path'], $item['rights'], $uid)) <= 0) {
         switch(-$res) {
-            case QN_R_READ:
+            case EQ_R_READ:
                 $missing = 'read';
                 break;
-            case QN_R_WRITE:
+            case EQ_R_WRITE:
                 $missing = 'write';
                 break;
             default:
                 $missing = '';
         }
-        throw new Exception("PHP or HTTP process has no {$missing} access on {$item['path']}", QN_ERROR_INVALID_CONFIG);
+        throw new Exception("PHP or HTTP process has no {$missing} access on {$item['path']}", EQ_ERROR_INVALID_CONFIG);
     }
 }
 
