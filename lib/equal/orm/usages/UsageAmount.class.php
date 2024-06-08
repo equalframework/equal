b9ce695c8e219837f4c497e6848cf61ad5f1f529
Cédric Françoys <cedricfrancoys@gmail.com>
2024-06-08 14:19:35 +0200

    fixed getScale() & getPrecision()

--- lib/equal/orm/usages/UsageAmount.class.php
+++ lib/equal/orm/usages/UsageAmount.class.php
@@ -12,19 +12,37 @@ use core\setting\Setting;
 class UsageAmount extends Usage {
 
     public function getScale(): int {
-        $scale = intval($this->getLength());
-        switch($this->getSubtype()) {
-            case 'money':
-                $scale = ($scale)?$scale:4;
-                break;
-            case 'percent':
-                $scale = ($scale)?$scale:6;
-                break;
-            case 'rate':
-                $scale = ($scale)?$scale:4;
-                break;
+        $scale = parent::getScale();
+        $precision = parent::getPrecision();
+        $length = parent::getLength();
+        // single number as length: use it as scale
+        if($length && $length == $precision) {
+            $scale = $length;
+        }
+        else {
+            $map_default = [
+                'money'     => 4,
+                'percent'   => 6,
+                'rate'      => 4
+            ];
+            $subtype = $this->getSubtype();
+            $scale = $map_default[$subtype] ?? 2;
         }
         return $scale;
+    }
+
+    public function getPrecision(): int {
+        $precision = parent::getPrecision();
+        $length = parent::getLength();
+        // single number as length means 'scale': use default precision
+        if($length && $length == $precision) {
+            $precision = 10;
+        }
+        else {
+            // use provided scale, fallback to default scale
+            $precision = ($precision)?$precision:2;
+        }
+        return $precision;
     }
 
     /**
