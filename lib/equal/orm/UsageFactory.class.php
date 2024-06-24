4cc7adb4466382fa553af3ea9f8387ccea1198aa
Cédric Françoys <cedricfrancoys@gmail.com>
2024-06-24 22:02:11 +0200

    changed create arg to $str_usage

--- lib/equal/orm/UsageFactory.class.php
+++ lib/equal/orm/UsageFactory.class.php
@@ -38,10 +38,10 @@ class UsageFactory {
      * Creates an instance of a Usage object based on a usage string descriptor.
      * @param string $usage     String representation of the usage (not an Usage instance).
      */
-    public static function create(string $usage): Usage {
+    public static function create(string $str_usage): Usage {
         // split parts and check usage string consistency
-        if(!preg_match('/([a-z]+)(\[([0-9]+)\])?\/?([-a-z0-9]*)(\.([-a-z0-9.]*))?(:(([-0-9a-z]*)\.?([0-9]*)))?({([0-9]+)(,([0-9]+))?})?/', $usage,  $matches)) {
-            throw new \Exception("invalid_usage for $usage", QN_ERROR_INVALID_PARAM);
+        if(!preg_match('/([a-z]+)(\[([0-9]+)\])?\/?([-a-z0-9]*)(\.([-a-z0-9.]*))?(:(([-0-9a-z]*)\.?([0-9]*)))?({([0-9]+)(,([0-9]+))?})?/', $str_usage,  $matches)) {
+            throw new \Exception("invalid_usage for $str_usage", QN_ERROR_INVALID_PARAM);
         }
 
         /*
@@ -63,58 +63,58 @@ class UsageFactory {
 
         switch($type) {
             case 'number':
-                $usageInstance = new UsageNumber($usage);
+                $usageInstance = new UsageNumber($str_usage);
                 break;
             case 'text':
-                $usageInstance = new UsageText($usage);
+                $usageInstance = new UsageText($str_usage);
                 break;
             case 'date':
-                $usageInstance = new UsageDate($usage);
+                $usageInstance = new UsageDate($str_usage);
                 break;
             case 'time':
-                $usageInstance = new UsageTime($usage);
+                $usageInstance = new UsageTime($str_usage);
                 break;
             case 'array':
-                $usageInstance = new UsageArray($usage);
+                $usageInstance = new UsageArray($str_usage);
                 break;
             case 'binary':
             // #memo - file and image types are deprecated
             case 'file':
             case 'image':
-                $usageInstance = new UsageBinary($usage);
+                $usageInstance = new UsageBinary($str_usage);
                 break;
 
             /* non-generic content-types */
             case 'amount':
-                $usageInstance = new UsageAmount($usage);
+                $usageInstance = new UsageAmount($str_usage);
                 break;
             case 'coordinate':
                 break;
             case 'country':
-                $usageInstance = new UsageCountry($usage);
+                $usageInstance = new UsageCountry($str_usage);
                 break;
             case 'currency':
-                $usageInstance = new UsageCurrency($usage);
+                $usageInstance = new UsageCurrency($str_usage);
                 break;
             case 'email':
-                $usageInstance = new UsageEmail($usage);
+                $usageInstance = new UsageEmail($str_usage);
                 break;
             case 'hash':
                 break;
             case 'language':
-                $usageInstance = new UsageLanguage($usage);
+                $usageInstance = new UsageLanguage($str_usage);
                 break;
             case 'password':
-                $usageInstance = new UsagePassword($usage);
+                $usageInstance = new UsagePassword($str_usage);
                 break;
             case 'phone':
-                $usageInstance = new UsagePhone($usage);
+                $usageInstance = new UsagePhone($str_usage);
                 break;
             case 'uri':
-                $usageInstance = new UsageUri($usage);
+                $usageInstance = new UsageUri($str_usage);
                 break;
             default:
-                $usageInstance = new Usage($usage);
+                $usageInstance = new Usage($str_usage);
         }
         return $usageInstance;
     }
