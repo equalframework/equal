5a58398f39a8e70545d6d4fa204afaee7756710d
Cédric Françoys <cedricfrancoys@gmail.com>
2024-06-26 10:52:17 +0200

    updated

--- packages/core/apps/welcome.php
+++ packages/core/apps/welcome.php
@@ -1,11 +1,11 @@
 <?php
 /*
-    This file is part of the eQual framework <http://www.github.com/cedricfrancoys/equal>
-    Some Rights Reserved, Cedric Francoys, 2010-2021
+    This file is part of the eQual framework <http://www.github.com/equalframework/equal>
+    Some Rights Reserved, Cedric Francoys, 2010-2024
     Licensed under GNU LGPL 3 license <http://www.gnu.org/licenses/>
 */
 list($params, $providers) = eQual::announce([
-    'description'   => 'Redirect to welcome public folder.',
+    'description'   => 'Redirect to `/welcome` public folder.',
     'params'        => [],
     'response'      => [
         'location'      => '/welcome/'
