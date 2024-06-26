7edf24c59fa28fe19950c63975c5b950677cdd2f
Cédric Françoys <cedricfrancoys@gmail.com>
2024-06-26 09:04:50 +0200

    added

---
+++ packages/core/apps/auth.php
@@ -0,0 +1,13 @@
+<?php
+/*
+    This file is part of the eQual framework <http://www.github.com/equalframework/equal>
+    Some Rights Reserved, Cedric Francoys, 2010-2024
+    Licensed under GNU LGPL 3 license <http://www.gnu.org/licenses/>
+*/
+list($params, $providers) = eQual::announce([
+    'description'   => 'Redirect to `/auth` public folder.',
+    'params'        => [],
+    'response'      => [
+        'location'      => '/auth/'
+    ]
+]);
