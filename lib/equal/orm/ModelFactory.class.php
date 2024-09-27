7c7b094c54cd761ed1b21a1d8dd3bc105af1060f
Lucas Laurent <lucas@yesbabylon.com>
2024-09-27 16:41:37 +0200

    Refacto to simplify

--- lib/equal/orm/ModelFactory.class.php
+++ lib/equal/orm/ModelFactory.class.php
@@ -12,107 +12,22 @@ use Exception;
  */
 class ModelFactory extends Service {
 
-    private $qty = 1;
-
-    private $values = [];
-
-    private $sequences = [];
-
     private $root_fields = ['id', 'creator', 'created', 'modifier', 'modified', 'deleted', 'state'];
 
+    private $relationship_field_types = ['many2one', 'one2many', 'many2many'];
+
     protected function __construct(Container $container) {
-    }
-
-    /**
-     * Sets the quantity of models to create for the next `ModelFactory::create()` function call
-     *
-     * @link ModelFactory::create()
-     * @throws Exception
-     */
-    public function qty(int $qty): ModelFactory {
-        if($qty < 1) {
-            throw new Exception('quantity_must_be_greater_than_zero', EQ_ERROR_INVALID_PARAM);
-        }
-
-        $this->qty = $qty;
-
-        return $this;
-    }
-
-    /**
-     * Sets the forced values of models to create for the next `ModelFactory::create()` function call
-     *
-     * @link ModelFactory::create()
-     * @throws Exception
-     */
-    public function values(array $values): ModelFactory {
-        if(!empty($this->sequences)) {
-            throw new Exception('cannot_set_values_and_sequences', EQ_ERROR_INVALID_PARAM);
-        }
-
-        foreach($values as $field => $value) {
-            if(!is_string($field)) {
-                throw new Exception('invalid_value_field_must_be_a_string', EQ_ERROR_INVALID_PARAM);
-            }
-            if(is_array($value) || is_object($value)) {
-                throw new Exception('invalid_value_not_expected_value', EQ_ERROR_INVALID_PARAM);
-            }
-        }
-
-        $this->values = $values;
-
-        return $this;
-    }
-
-    /**
-     * Sets sequences of forced values of models to create for the next `ModelFactory::create()` function call
-     *
-     * @link ModelFactory::create()
-     * @throws Exception
-     */
-    public function sequences(array $sequences): ModelFactory {
-        if(!empty($this->values)) {
-            throw new Exception('cannot_set_values_and_sequences', EQ_ERROR_INVALID_PARAM);
-        }
-
-        foreach($sequences as $index => $values) {
-            if(!is_int($index)) {
-                throw new Exception('invalid_sequence_index_must_be_integer', EQ_ERROR_INVALID_PARAM);
-            }
-
-            foreach($values as $field => $value) {
-                if(!is_string($field)) {
-                    throw new Exception('invalid_sequence_field_must_be_a_string', EQ_ERROR_INVALID_PARAM);
-                }
-                if(is_array($value) || is_object($value)) {
-                    throw new Exception('invalid_sequence_not_expected_value', EQ_ERROR_INVALID_PARAM);
-                }
-            }
-        }
-
-        $this->sequences = $sequences;
-
-        return $this;
-    }
-
-    private function getIncrementedSequenceIndex(int $sequence_index): int {
-        $sequence_index++;
-        if(!isset($this->sequences[$sequence_index])) {
-            $sequence_index = 0;
-        }
-
-        return $sequence_index;
     }
 
     /**
      * Returns one or multiple generated object(s) using the given class' model schema
      *
      * @param class-string $class
-     * @see ModelFactory::qty() To set the quantity of objects to create (default: `1`)
-     * @see ModelFactory::values() To set the forced values of objects to create (default: `[]`)
+     * @param array $options Factory options like qty, values and sequences
+     * @return array
      * @throws Exception
      */
-    public function create(string $class): array {
+    public function create(string $class, array $options = []): array {
         /** @var ObjectManager $orm */
         $orm = $this->container->get('orm');
 
@@ -122,35 +37,66 @@ class ModelFactory extends Service {
         }
 
         $entities = [];
+
         $sequence_index = 0;
-        $values = !empty($this->sequences) ? $this->sequences[0] : $this->values;
+        $values = !empty($options['sequences']) ? $options['sequences'][0] : ($options['values'] ?? []);
+
+        if(!isset($options['qty'])) {
+            $qty = 1;
+        }
+        elseif(is_array($options['qty'])) {
+            $qty = mt_rand($options['qty'][0], $options['qty'][1]);
+        }
+        else {
+            $qty = $options['qty'];
+        }
 
         $schema = $model->getSchema();
-        for($i = 1; $i <= $this->qty; $i++) {
-            $entities[] = $this->createEntityFromModelSchema($schema, $values);
+        for($i = 1; $i <= $qty; $i++) {
+            $entities[] = $this->createEntityFromModelSchema($schema, $values, $options['relationships'] ?? []);
 
-            if(!empty($this->sequences)) {
-                $sequence_index = $this->getIncrementedSequenceIndex($sequence_index);
-                $values = $this->sequences[$sequence_index];
+            if(!empty($options['sequences'])) {
+                $sequence_index = isset($options['sequences'][++$sequence_index]) ? $sequence_index : 0;
+
+                $values = $options['sequences'][$sequence_index];
             }
         }
 
-        $this->resetProperties();
-
-        return count($entities) === 1 ? $entities[0] : $entities;
+        return $entities;
     }
 
     /**
      * @throws Exception
      */
-    private function createEntityFromModelSchema(array $model_schema, array $forced_values = []): array {
+    private function createEntityFromModelSchema(array $model_schema, array $forced_values = [], array $relationships = []): array {
         $object = [];
         foreach($model_schema as $field => $field_descriptor) {
+            $field_type = $field_descriptor['result_type'] ?? $field_descriptor['type'];
             if(array_key_exists($field, $forced_values)) {
                 $object[$field] = $forced_values[$field];
                 continue;
             }
-            elseif(in_array($field, $this->root_fields)) {
+            elseif(
+                in_array($field_type, $this->relationship_field_types)
+                && (in_array($field, $relationships) || array_key_exists($field, $relationships))
+            ) {
+                if($field_type === 'many2one') {
+                    $factory_options = $relationships[$field] ?? [];
+                    if(isset($factory_options['qty']) && $factory_options['qty'] !== 1) {
+                        $factory_options['qty'] = 1;
+                    }
+
+                    $object[$field] = $this->create($field_descriptor['foreign_object'], $factory_options)[0];
+                } else {
+                    $object[$field] = $this->create($field_descriptor['foreign_object'], $relationships[$field] ?? []);
+                }
+            }
+
+            if(
+                isset($object[$field])
+                || in_array($field, $this->root_fields)
+                || in_array($field_type, $this->relationship_field_types)
+            ) {
                 continue;
             }
 
@@ -164,10 +110,5 @@ class ModelFactory extends Service {
         }
 
         return $object;
-    }
-
-    private function resetProperties(): void {
-        $this->qty = 1;
-        $this->values = [];
     }
 }
