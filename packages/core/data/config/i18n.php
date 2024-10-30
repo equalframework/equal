11ff0e412d7d4a292b4ba674047d9acb44123c6b
Cédric Françoys <cedricfrancoys@gmail.com>
2024-10-30 15:35:06 +0100

    changed to relay to use core_translation

--- packages/core/data/config/i18n.php
+++ packages/core/data/config/i18n.php
@@ -4,9 +4,8 @@
     Some Rights Reserved, Cedric Francoys, 2010-2021
     Licensed under GNU GPL 3 license <http://www.gnu.org/licenses/>
 */
-// #deprecated - use `core_config_translation` instead
 list($params, $providers) = eQual::announce([
-    'description'   => "Retrieves the translation values related to the specified entity.",
+    'description'   => "Retrieves the translation values related to the specified entity. This controller is deprecated, use `core_config_translation` instead.",
     'deprecated'    => true,
     'params'        => [
         'entity' =>  [
@@ -26,65 +25,13 @@ list($params, $providers) = eQual::announce([
         'charset'       => 'utf-8',
         'accept-origin' => '*'
     ],
-    'providers'     => ['context', 'orm']
+    'providers'     => ['context']
 ]);
 
-list($context, $orm) = [ $providers['context'], $providers['orm'] ];
+['context' => $context] = $providers;
 
-$entity = $params['entity'];
-$parents = [];
-
-// for non-controller entities, retrieve parents hierarchy
-$parts = explode('\\', $params['entity']);
-$file = array_pop($parts);
-if(!ctype_lower(substr($file, 0, 1))) {
-    while(true) {
-        $parent = get_parent_class($entity);
-        if(!$parent) break;
-        if($parent == 'equal\orm\Model') {
-            // simulate Model class to be part of core package (to fallback to related translation files)
-            $parents[] = 'core\Model';
-        }
-        else {
-            $parents[] = $parent;
-        }
-        $entity = $parent;
-    }
-}
-
-$parents[] = $params['entity'];
-
-// init resulting lang schema
-$lang = [];
-
-foreach($parents as $entity) {
-    $parts = explode('\\', $entity);
-    $package = array_shift($parts);
-
-    $class_dir = implode('/', $parts);
-    $file = EQ_BASEDIR."/packages/{$package}/i18n/{$params['lang']}/{$class_dir}.json";
-
-    if(!file_exists($file)) {
-        continue;
-    }
-
-    if(($schema = json_decode(@file_get_contents($file), true)) === null) {
-        throw new Exception("malformed_json", QN_ERROR_INVALID_CONFIG);
-    }
-    if(empty($lang)) {
-        $lang = $schema;
-    }
-    else {
-        $lang = array_replace_recursive($lang, $schema);
-    }
-}
-
-/*
-if(empty($lang)) {
-    throw new Exception("unknown_lang_file", QN_ERROR_UNKNOWN_OBJECT);
-}
-*/
+$result = eQual::run('get', 'translation', $params);
 
 $context->httpResponse()
-        ->body($lang)
+        ->body($result)
         ->send();
