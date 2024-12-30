2a5e8fdbd84e6ba383fb51e3b698182ece2d30bd
Cédric Françoys <cedricfrancoys@gmail.com>
2024-12-30 09:22:43 +0100

    minor renamed var

--- packages/core/data/user/passkey-register-options.php
+++ packages/core/data/user/passkey-register-options.php
@@ -60,9 +60,9 @@ elseif($cross_platform_attachment === 'platform') {
     $cross_platform_attachment = false;
 }
 
-$formats = ['android-key', 'android-safetynet', 'apple', 'fido-u2f', 'none', 'packed', 'tpm'];
+$passkey_formats = ['android-key', 'android-safetynet', 'apple', 'fido-u2f', 'none', 'packed', 'tpm'];
 $allowed_formats = [];
-foreach($formats as $format) {
+foreach($passkey_formats as $format) {
     $is_format_allowed = Setting::get_value('core', 'security', "passkey_format_$format", true);
     if($is_format_allowed) {
         $allowed_formats[] = $format;
