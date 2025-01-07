ad3b4f89b480234257d0a77d5ba31be5e48c92cc
Cédric Françoys <cedricfrancoys@gmail.com>
2025-01-07 13:05:15 +0100

    made setting passkey_user-handle optional

--- packages/core/data/signin-info.php
+++ packages/core/data/signin-info.php
@@ -76,32 +76,31 @@ $passkey_creation = Setting::get('core', 'security', 'passkey_creation', $global
 $user_handle = Setting::get('core', 'security', 'passkey_user-handle', null, ['user_id' => $user['id']]);
 
 if(!$user_handle) {
+    // generate temporary anonymous user_handle
+    $user_handle = bin2hex(random_bytes(16));
+
     $setting = Setting::search(['name', '=', 'core.security.passkey_user-handle'])
         ->read(['id'])
         ->first();
 
-    if(!$setting) {
-        throw new Exception('missing_setting', EQ_ERROR_INVALID_CONFIG);
-    }
+    if($setting) {
+        // make sure the handle is not already assigned
+        while(true) {
+            $values = SettingValue::search([
+                    ['setting_id', '=', $setting['id']],
+                    ['value', '=', $user_handle]]
+                )
+                ->get();
 
-    // generate temporary anonymous user_handle
-    $user_handle = bin2hex(random_bytes(16));
-
-    // make sure the handle is not already assigned
-    while(true) {
-        $values = SettingValue::search([
-                ['setting_id', '=', $setting['id']],
-                ['value', '=', $user_handle]]
-            )
-            ->get();
-
-        if(!count($values)) {
-            break;
+            if(!count($values)) {
+                break;
+            }
+            $user_handle = bin2hex(random_bytes(16));
         }
-        $user_handle = bin2hex(random_bytes(16));
+
+        Setting::set_value('core', 'security', 'passkey_user-handle', $user_handle, ['user_id' => $user['id']]);
     }
 
-    Setting::set_value('core', 'security', 'passkey_user-handle', $user_handle, ['user_id' => $user['id']]);
 }
 
 $context->httpResponse()
