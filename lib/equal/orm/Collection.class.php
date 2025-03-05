d541df0605854499dcbd1671cfe092229d3b45c9
Cédric Françoys <cedricfrancoys@gmail.com>
2025-03-05 15:16:45 +0100

    relayed exception in Collection::call()

--- lib/equal/orm/Collection.class.php
+++ lib/equal/orm/Collection.class.php
@@ -705,7 +705,7 @@ class Collection implements \Iterator, \Countable {
         }
         catch(\Exception $e) {
             trigger_error("ORM::unexpected error when calling {$method_name} on {$this->class}: ".$e->getMessage(), EQ_REPORT_ERROR);
-            throw new \Exception('method_call_failed', EQ_ERROR_UNKNOWN);
+            throw $e;
         }
 
         return $result;
