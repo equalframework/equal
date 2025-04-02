0e9ed625ee9bd74d76104f9635b1c963e6810046
anissaaab <60183@etu.he2b.be>
2025-04-02 11:16:36 +0200

    roles, policies, actions controller

---
+++ packages/core/actions/config/create-policies.php
@@ -0,0 +1,73 @@
+<?php
+/*
+    This file is part of the eQual framework <http://www.github.com/equalframework/equal>
+    Some Rights Reserved, eQual framework, 2010-2024
+    Original author(s): Cédric FRANCOYS
+    Licensed under GNU LGPL 3 license <http://www.gnu.org/licenses/>
+*/
+
+list($params, $providers) = eQual::announce([
+    'description'   => "Add an empty actions to the given class by creating a `getActions()` method (if not defined yet).",
+    'params'        => [
+        'entity' =>  [
+            'description'   => 'Full name (including namespace) of the class to look into (e.g. \'core\\User\').',
+            'type'          => 'string',
+            'required'      => true
+        ]
+    ],
+    'response'      => [
+        'content-type'  => 'text',
+        'charset'       => 'utf-8',
+        'accept-origin' => '*'
+    ],
+    'access' => [
+        'visibility'        => 'protected',
+        'groups'            => ['admins']
+    ],
+    'providers'     => ['context', 'orm']
+]);
+
+/**
+ * @var \equal\php\Context               $context
+ * @var \equal\orm\ObjectManager         $orm
+ */
+list($context, $orm) = [ $providers['context'], $providers['orm'] ];
+
+// force class autoload
+$entity = $orm->getModel($params['entity']);
+if(!$entity) {
+    throw new Exception("unknown_entity", QN_ERROR_INVALID_PARAM);
+}
+
+$class = new ReflectionClass($entity::getType());
+if($class->getMethod('getPolicies')->class == $entity::getType()) {
+    throw new Exception("duplicate_method", QN_ERROR_INVALID_PARAM);
+}
+
+$file = $class->getFileName();
+$code = file_get_contents($file);
+
+// generate replacement code for the  getPolicies method
+$policies_code = ''.
+    "    public static function getPolicies() {\n".
+    "        return [];\n".
+    "    }\n".
+    "\n";
+
+// find the closing curly-bracket of the class
+$pos = strrpos($code, '}');
+
+if($pos === false) {
+    throw new Exception('malformed_file', QN_ERROR_UNKNOWN);
+}
+
+$result = substr_replace($code, $policies_code, $pos, 0);
+
+// write back the code to the source file
+if(file_put_contents($file, rtrim($result)."\n") === false) {
+    throw new Exception('io_error', QN_ERROR_UNKNOWN);
+}
+
+$context->httpResponse()
+        ->status(204)
+        ->send();
---
+++ packages/core/actions/config/create-policy.php
@@ -0,0 +1,58 @@
+<?php
+use PhpParser\{Node, NodeTraverser, NodeVisitorAbstract, ParserFactory, NodeFinder};
+use equal\orm\Entity;
+list($params, $providers) = eQual::announce([
+    'description'   => "create a new policy",
+    'help'          => "This controller relies on the PHP binary. Ensure the PHP binary is present in the PATH.",
+    'params'        => [
+        'entity'    => [
+            'description'   => 'Name of the entity (class).',
+            'type'          => 'string',
+            'required'      => true
+        ],
+        'name'    => [
+            'description'   => 'Name of  the policy',
+            'type'          => 'string',
+            'required'      => true
+        ]
+    ],
+    'response'      => [
+        'content-type'  => 'text/plain',
+        'charset'       => 'UTF-8',
+        'accept-origin' => '*'
+    ],
+    'providers'     => ['context', 'orm']
+]);
+
+/**
+ * @var \equal\php\Context          $context
+ * @var \equal\orm\ObjectManager    $orm
+ */
+
+list($context, $orm) = [$providers['context'], $providers['orm']];
+
+
+$entity = new Entity($params['entity']);
+$new_policy = $params['name'];
+// force class autoload
+$entityInstance = $orm->getModel($entity->getFullName());
+if(!$entityInstance) {
+    throw new Exception("unknown_entity", QN_ERROR_INVALID_PARAM);
+}
+
+$method_name = 'getPolicies' ;
+$structure= [
+'description' =>'',
+'function' => ''
+];
+$class = new ReflectionClass($entityInstance::getType());
+if(!($class->getMethod($method_name)->class == $entityInstance::getType())) {
+    equal::run('do','core_config_create-policies',['entity' => $entity->getFullName()], true);
+}
+
+$entity->updateMethodLine($method_name,$new_policy, $structure);
+
+
+$context->httpResponse()
+        ->status(204)
+        ->send();
