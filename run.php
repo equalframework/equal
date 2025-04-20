ea8d069450a767670122777087215db1dfd11092
Cédric Françoys <cedricfrancoys@gmail.com>
2025-04-20 21:00:50 +0200

    limited test for route operation presence to  &

--- run.php
+++ run.php
@@ -172,7 +172,7 @@ try {
             ], JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES)
         );
 
-    if($route['operation']['name'] === 'core') {
+    if($route['operation']['name'] === 'core' && $route['operation']['name'] !== 'show') {
         throw new Exception("No controller provided for request type `{$route['operation']['type']}`.", EQ_ERROR_MISSING_PARAM);
     }
 
