443db73d2536d0d161b56a423889fa252fde7d88
Cédric Françoys <cedricfrancoys@gmail.com>
2025-05-16 10:08:36 +0200

    added

---
+++ packages/core/data/json-validate.php
@@ -0,0 +1,66 @@
+<?php
+/*
+    This file is part of the eQual framework <http://www.github.com/equalframework/equal>
+    Some Rights Reserved, eQual framework, 2010-2024
+    Original author(s): Cédric FRANCOYS
+    Licensed under GNU LGPL 3 license <http://www.gnu.org/licenses/>
+*/
+use Opis\JsonSchema\Errors\ErrorFormatter;
+use Opis\JsonSchema\ValidationResult;
+use Opis\JsonSchema\Validator;
+
+
+[$params, $providers] = eQual::announce([
+    'description'   => "Parse and validate a raw JSON structure against a given JSON-schema.",
+    'params'        => [
+        'json' =>  [
+            'type'          => 'string',
+            'usage'         => 'text/json.medium',
+            'description'   => "Raw JSON to be validated against a given schema.",
+            'required'      => true
+        ],
+        'schema_id' =>  [
+            'type'          => 'string',
+            'pattern'       => '/^urn:[a-z0-9_-]+:json-schema:[a-z0-9_-]+:[a-z0-9._-]+$/i',
+            'description'   => 'Json-Schema $id following the syntax `urn:<namespace>:json-schema:<package>:<schema-name>`.',
+            'help'          => "Target JSON schema is expected to be a https://json-schema.org/draft/2020-12/ descriptor for validation.",
+            'required'      => true
+        ]
+    ],
+    'access' => [
+        'visibility'        => 'protected'
+    ],
+    'response'      => [
+        'content-type'  => 'application/json',
+        'charset'       => 'utf-8',
+        'accept-origin' => '*'
+    ],
+    'providers'     => ['context']
+]);
+
+/**
+ * @var \equal\php\Context               $context
+ */
+$context = $providers['context'];
+
+$schema = eQual::run('get', 'json-schema', ['id' => $params['schema_id']], false, true);
+
+$validator = new Validator();
+$validator->setMaxErrors(10);
+
+/** @var ValidationResult $result */
+$result = $validator->validate(json_decode($params['json']), $schema);
+
+$valid = $result->isValid();
+$errors = [];
+
+if(!$valid) {
+    $errors = (new ErrorFormatter())->format($result->error());
+}
+
+$context->httpResponse()
+        ->body([
+            'result' => $valid,
+            'errors' => $errors
+        ])
+        ->send();
