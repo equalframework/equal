b1666062c41bf62a0faa7f917688015c7c5f783e
Cédric Françoys <cedricfrancoys@gmail.com>
2025-05-21 09:28:33 +0200

    extended phone regex to match all syntaxes

--- lib/equal/orm/usages/UsagePhone.class.php
+++ lib/equal/orm/usages/UsagePhone.class.php
@@ -19,15 +19,28 @@ class UsagePhone extends Usage {
         }
     }
 
-    public function getConstraints(): array {
-        return [
-            'invalid_phone' => [
-                'message'   => 'String does not comply with usage constraint.',
-                'function'  =>  function($value) {
-                    return (bool) (preg_match('/^((\+[1-9]{2,3})|00|0)+([0-9]){1,13}$/', $value));
-                }
-            ]
-        ];
+    /**
+     * Validates a phone number in a normalized format (only digits and an optional + or 00 prefix).
+     *
+     * Supported formats:
+     * - International E.164:           +32478456789
+     * - International alternative:     0032478456789
+     * - National with leading zero:    0478456789
+     * - Special national numbers:      080019400, 116000
+     *
+     * Rejected examples:
+     * - Too short / ambiguous:         123
+     * - Not normalized (contains punctuation/spaces): +32 (478) 45.67.89
+     */
+     public function getConstraints(): array {
+         return [
+             'invalid_phone' => [
+                 'message'   => 'String does not comply with phone format.',
+                 'function'  =>  function($value) {
+                    return (bool) (preg_match('/^(\+|00)?[1-9][0-9]{6,14}$|^0[1-9][0-9]{5,13}$|^([1-9][0-9]{3,6})$/', $value));
+                 }
+             ]
+         ];
     }
 
     public function generateRandomValue(): string {
