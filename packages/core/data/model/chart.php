fabc771743de5dce69acf9f66effa200498dd9d4
Lucas <lucas@yesbabylon.com>
2025-08-25 16:12:22 +0200

    fix error redeclared method

--- packages/core/data/model/chart.php
+++ packages/core/data/model/chart.php
@@ -90,6 +90,31 @@ list($params, $providers) = eQual::announce([
 
 ['context' => $context, 'orm' => $orm] = $providers;
 
+$getDateIndex = function($date, $interval) {
+    switch($interval) {
+        case 'week':
+            return date('Y-W', $date);
+        case 'month':
+            return date('Y-m', $date);
+        case 'year':
+        default:
+            return date('Y', $date);
+    }
+};
+
+$getNextDate = function($date, $interval) {
+    switch($interval) {
+        case 'week':
+            $day = date("w", $date);
+            $day = ($day == 0)?7:$day;
+            return $date + ((8-$day)*24*3600);
+        case 'month':
+            return strtotime(date("Y-m-t", $date)) + (48*3600);
+        case 'year':
+        default:
+            return strtotime( (date('Y', $date) + 1).'-01-02');
+    }
+};
 
 // retrieve target entity
 $entity = $orm->getModel($params['entity']);
@@ -163,9 +188,9 @@ $results_map = [];
 if($params['group_by'] == 'range') {
     $date = $params['range_from'];
     while($date < $params['range_to']) {
-        $index = _get_date_index($date, $params['range_interval']);
+        $index = $getDateIndex($date, $params['range_interval']);
         $results_map[$index] = [];
-        $date = _get_next_date($date, $params['range_interval']);
+        $date = $getNextDate($date, $params['range_interval']);
     }
 }
 else {
@@ -209,7 +234,7 @@ foreach($datasets as $index => $dataset) {
         foreach($objects as $oid => $object) {
             if(in_array($schema[$params['field']]['type'], ['date', 'datetime'])) {
                 if($params['group_by'] == 'range') {
-                    $group_index = _get_date_index($object[$params['field']], $params['range_interval']);
+                    $group_index = $getDateIndex($object[$params['field']], $params['range_interval']);
                 }
                 else {
                     // #todo - check value of param 1
@@ -285,28 +310,3 @@ if($params['mode'] == 'grid') {
 $context->httpResponse()
         ->body($result)
         ->send();
-
-
-function _get_date_index($date, $interval) {
-    switch($interval) {
-        case 'week':
-            return date('Y-W', $date);
-        case 'month':
-            return date('Y-m', $date);
-        case 'year':
-            return date('Y', $date);
-    }
-}
-
-function _get_next_date($date, $interval) {
-    switch($interval) {
-        case 'week':
-            $day = date("w", $date);
-            $day = ($day == 0)?7:$day;
-            return $date + ((8-$day)*24*3600);
-        case 'month':
-            return strtotime(date("Y-m-t", $date)) + (48*3600);
-        case 'year':
-            return strtotime( (date('Y', $date) + 1).'-01-02');
-    }
-}
