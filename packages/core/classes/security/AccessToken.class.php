e6b04f35c221bad328ae33a29ad7d42d689fd307
Cédric Françoys <cedricfrancoys@gmail.com>
2026-04-27 11:51:49 +0200

    added

---
+++ packages/core/classes/security/AccessToken.class.php
@@ -0,0 +1,56 @@
+<?php
+/*
+    This file is part of the eQual framework <http://www.github.com/equalframework/equal>
+    Some Rights Reserved, eQual framework, 2010-2024
+    Original author(s): Cédric FRANCOYS
+    Licensed under GNU GPL 3 license <http://www.gnu.org/licenses/>
+*/
+namespace core\security;
+
+use equal\orm\Model;
+
+class AccessToken extends Model {
+
+    public static function getDescription() {
+        return 'Access Token.';
+    }
+
+    public static function getColumns() {
+        return [
+            'user_id' => [
+                'type'              => 'many2one',
+                'relation'          => 'core\User',
+                'required'          => true,
+                'description'       => 'User associated with the token.'
+            ],
+
+            'token_type' => [
+                'type'              => 'string',
+                'selection'         => [
+                    'access_token',
+                    'refresh_token'
+                ],
+                'description'       => 'Type of token (access or refresh).'
+            ],
+
+            'token' => [
+                'type'              => 'string',
+                'usage'             => 'text/plain.small',
+                'description'       => "User the role is assigned to."
+            ],
+
+            'expiration' => [
+                'type'              => 'datetime',
+                'description'       => 'Token expiration date.'
+            ],
+
+            'is_revoked' => [
+                'type'              => 'boolean',
+                'default'           => false,
+                'description'       => 'Indicates if the token has been revoked.'
+            ],
+
+        ];
+    }
+
+}
