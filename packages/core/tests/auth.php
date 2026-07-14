977bae55ea6cd16bf05465584367c38d6ba094e0
Cédric Françoys <cedricfrancoys@gmail.com>
2026-07-14 11:34:28 +0200

    added tests 0103 & 0104

--- packages/core/tests/auth.php
+++ packages/core/tests/auth.php
@@ -34,5 +34,37 @@ $tests = [
                     ]);
                     return $providers['@@testAuth'];
                 }
+        ],
+
+    '0103' => [
+            'description'       =>  'Retrieve a valid access token payload.',
+            'return'            =>  ['array'],
+            'assert'            =>  function($payload) {
+                    return isset($payload['id'], $payload['exp'])
+                        && $payload['id'] === EQ_ROOT_USER_ID
+                        && $payload['exp'] > time();
+                },
+            'act'               =>  function () {
+                    list($params, $providers) = eQual::announce([
+                        'providers' => ['equal\auth\AuthenticationManager']
+                    ]);
+                    $auth = $providers['equal\auth\AuthenticationManager'];
+
+                    return $auth->retrieveAccessToken($auth->token(EQ_ROOT_USER_ID, 60));
+                }
+        ],
+
+    '0104' => [
+            'description'       =>  'Do not retrieve an expired access token payload.',
+            'return'            =>  ['NULL'],
+            'expected'          =>  null,
+            'act'               =>  function () {
+                    list($params, $providers) = eQual::announce([
+                        'providers' => ['equal\auth\AuthenticationManager']
+                    ]);
+                    $auth = $providers['equal\auth\AuthenticationManager'];
+
+                    return $auth->retrieveAccessToken($auth->token(EQ_ROOT_USER_ID, -60));
+                }
         ]
 ];
